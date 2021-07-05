9e1fb0b833f01031e804c6389ea17c6ae7bc22af
Zain Fathoni <me@zainf.dev>
2021-07-05 23:12:47 +0800

    Add pre-commit hook using lint-staged and husky
    npx mrm lint-staged
    https://prettier.io/docs/en/precommit.html#option-1-lint-stagedhttpsgithubcomokonetlint-staged

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Warga Bantu Warga</title>
-        <meta name="description" content="Database ketersediaan bed di RS, donor plasma, dan oksigen" />
+        <meta
+          name="description"
+          content="Database ketersediaan bed di RS, donor plasma, dan oksigen"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <iframe
         id="wbw"
         height="100%"
         width="100%"
-        src="https://docs.google.com/document/d/e/2PACX-1vR0xm-hYs5m4smcaA20vo6SgGYgQm-nae-JZku2WAyv8HK5PiE-GrjtvM87e9Kr_rZ2YLd10_gz6reT/pub">
-      </iframe>
+        src="https://docs.google.com/document/d/e/2PACX-1vR0xm-hYs5m4smcaA20vo6SgGYgQm-nae-JZku2WAyv8HK5PiE-GrjtvM87e9Kr_rZ2YLd10_gz6reT/pub"
+      ></iframe>
     </div>
-  )
+  );
 }
