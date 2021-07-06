b5b06652a5548c5acf95b2a99becb6c64cfcfec4
Zain Fathoni <me@zainf.dev>
2021-07-06 22:37:15 +0800

    Update the Google Docs source link to the kcov.id URL shortener

--- etc/fetchers/fetch-wbw.js
+++ etc/fetchers/fetch-wbw.js
@@ -4,9 +4,7 @@ const path = require("path");
 const fetch = require("node-fetch");
 
 async function fetchWbw() {
-  const source = await fetch(
-    "https://docs.google.com/document/d/e/2PACX-1vR0xm-hYs5m4smcaA20vo6SgGYgQm-nae-JZku2WAyv8HK5PiE-GrjtvM87e9Kr_rZ2YLd10_gz6reT/pub"
-  );
+  const source = await fetch("https://kcov.id/wbw-source");
   const $ = cheerio.load(await source.text());
 
   fs.writeFileSync(
