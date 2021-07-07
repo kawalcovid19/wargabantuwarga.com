0f7ec73aab3e9436f3c6169a242a51b3cfeaee16
Griko Nibras <grikomsn@icloud.com>
2021-07-07 19:02:58 +0700

    Enable experimental features

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+/** @type {import("next/dist/next-server/server/config-shared").NextConfig} */
 module.exports = {
+  // https://github.com/vercel/next.js/blob/canary/packages/next/server/config-shared.ts#L42-L65
+  experimental: {
+    optimizeCss: true,
+    optimizeImages: true,
+    scrollRestoration: true,
+    workerThreads: true,
+  },
+
+  // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   reactStrictMode: true,
 };
