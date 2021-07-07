2a07fad7f53a7d666fe66b6b97c518383f35d0e2
Griko Nibras <grikomsn@icloud.com>
2021-07-07 18:12:54 +0700

    Add missing getInitialProps in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html lang="id">
