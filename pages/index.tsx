cd988b2cd1c035e7eb3fc2c0a2f370229b1cd5f6
Zain Fathoni <me@zainf.dev>
2021-07-09 00:28:14 +0800

    Wrap image element within a h1 tag as the title of the page
    closes [3] Wrap the hero image with h1 #27

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-// import Image from "next/image";
-// import hero from "../public/hero.png";
 
 type Props = {
   html: string;
@@ -51,15 +49,16 @@ export default function Home(props: Props) {
       </Head>
       <main>
         <header>
-          {/* <Image src={hero} alt="Warga Bantu Warga" /> */}
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="https://firebase-kanvas.imgix.net/warga_bantu_warga/hero_banner.png?auto=format,compress,enhance&fm=pjpg&cs=tinysrgb&fit=scale"
-            alt="Warga Bantu Warga"
-            height="291"
-            width="650"
-            style={{ maxWidth: 650, height: "auto", width: "100%" }}
-          />
+          <h1>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://firebase-kanvas.imgix.net/warga_bantu_warga/hero_banner.png?auto=format,compress,enhance&fm=pjpg&cs=tinysrgb&fit=scale"
+              alt="Warga Bantu Warga"
+              height="291"
+              width="650"
+              style={{ maxWidth: 650, height: "auto", width: "100%" }}
+            />
+          </h1>
         </header>
         <article dangerouslySetInnerHTML={{ __html: props.html }}></article>
       </main>
