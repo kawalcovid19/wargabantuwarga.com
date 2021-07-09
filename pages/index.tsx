8659d4458b70d229c1ea3c617e8df51bd6b27db3
Zain Fathoni <me@zainf.dev>
2021-07-09 20:38:47 +0800

    Add the script tag properly

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,13 +16,11 @@ export default function Home(props: Props) {
   return (
     <>
       <Head>
-        <Script>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script>{`<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5X4ZPBX');`}
-        </Script>
+})(window,document,'script','dataLayer','GTM-5X4ZPBX');</script>`}</Script>
         <title>{meta.title}</title>
         <meta name="description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
