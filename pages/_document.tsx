1d89f79a6427747167b4f57d16bd423e0051cdaf
Zain Fathoni <me@zainf.dev>
2021-07-09 18:57:15 +0800

    Simplify MyDocument export statement

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="id">
@@ -13,5 +13,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
