4cd597ea0c5a5d99c47f2b6583db6e65dd8fa8ba
Zain Fathoni <me@zainf.dev>
2021-07-09 18:50:34 +0800

    Simplify App export statement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import "../styles/fonts.css";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-export default MyApp;
