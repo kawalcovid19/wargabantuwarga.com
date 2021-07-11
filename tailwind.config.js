31aa3098cbd6f539022630c90528806db45078b6
Hani Husamuddin <hani.husam@gmail.com>
2021-07-11 08:22:07 +0700

    setup app's theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{ts,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      currentColor: "currentColor",
+      black: "#121212",
+      gray: {
+        DEFAULT: "#dedede",
+        dark: "#aaaaaa",
+      },
+      borderColor: (theme) => ({
+        ...theme("colors"),
+        DEFAULT: theme("colors.gray.DEFAULT", "currentColor"),
+      }),
+    },
+    fontFamily: {
+      sans: ["Nunito", "sans-serif"],
+    },
     extend: {},
   },
   variants: {
