c5152271302e8ead89764cae50a0b9f87221d24f
Hani Husamuddin <hani.husam@gmail.com>
2021-07-11 21:25:14 +0700

    resolve suggestion comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
@@ -12,10 +14,11 @@ module.exports = {
         dark: "#aaaaaa",
       },
     },
-    fontFamily: {
-      sans: ["Nunito", "sans-serif"],
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {},
