bca857f8c9180c46cb74bb8fa5a53fe91c0a2221
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-07-11 21:10:54 +0700

    Sync with new Github Workflows

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import NextScript from "next/script";
 
 import { Script } from "../components/script";
 import config from "../lib/config";
--- sentry.client.config.js
+++ sentry.client.config.js
@@ -4,12 +4,10 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
