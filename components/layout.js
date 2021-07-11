9c5c8adabed005bbf01ae7d070aadc50084be5d5
Zain Fathoni <me@zainf.dev>
2021-07-11 14:51:28 +0800

    Remove unnecessary next/link usages

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,5 @@
-import Link from "next/link";
-
 export const Layout = ({ children }) => (
   <>
-    <nav>
-      <Link href="/">
-        <a>home</a>
-      </Link>
-      <Link href="/blog">
-        <a>blog</a>
-      </Link>
-      <Link href="/about">
-        <a>about</a>
-      </Link>
-    </nav>
     <main>{children}</main>
     <style jsx>{`
       nav {
