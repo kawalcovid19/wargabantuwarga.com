0cd574dff7b29464bcb9a9d644e3cf106f6fbc53
Zain Fathoni <me@zainf.dev>
2021-07-12 20:50:43 +0800

    Convert name into a standardized slug

---
+++ lib/string-utils.ts
@@ -0,0 +1,19 @@
+export function replaceSpacesWithCamelCase(str: string): string {
+  return str.replace(/\s+/g, (s) => {
+    return s.substring(0, 1).toUpperCase() + s.substring(1);
+  });
+}
+
+export function removeSpaces(str: string): string {
+  return str.replace(/\s+/g, "");
+}
+
+export function convertToKebabCase(str: string): string {
+  return str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+}
+
+export function composeFunctions(...functions: Function[]): Function {
+  return (args: any) => {
+    return functions.reduce((acc, fn) => fn(acc), args);
+  };
+}
--- pages/database/[slug].tsx
+++ pages/database/[slug].tsx
@@ -1,22 +1,43 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import database from "../../lib/database";
+import {
+  composeFunctions,
+  convertToKebabCase,
+  removeSpaces,
+  replaceSpacesWithCamelCase,
+} from "../../lib/string-utils";
 
 type DatabaseProps = {
   title: string;
 };
 
 export default function Database(props: DatabaseProps) {
+  const title = composeFunctions(
+    replaceSpacesWithCamelCase,
+    removeSpaces,
+    convertToKebabCase
+  )(props.title);
+
   return (
     <main>
-      <h1>Database for {props.title}</h1>
+      <h1>Database for {title}</h1>
     </main>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const paths = database.map((item) => ({
-    params: { slug: item.name },
-  }));
+  const paths = database.map((item, index) => {
+    const name = composeFunctions(
+      replaceSpacesWithCamelCase,
+      removeSpaces,
+      convertToKebabCase
+    )(item.name);
+    const slug = `${name}-${index}`;
+    console.log(slug);
+    return {
+      params: { slug },
+    };
+  });
 
   return {
     paths,
@@ -26,10 +47,9 @@ export const getStaticPaths: GetStaticPaths = () => {
 
 export const getStaticProps: GetStaticProps = ({ params = {} }) => {
   const { slug } = params;
-  const title = slug;
   return {
     props: {
-      title,
+      title: slug,
     },
   };
 };
