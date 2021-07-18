326395a72bd99aacef860caeac46d1f491dcd6e9
Resi Respati <resir014@gmail.com>
2021-07-18 12:52:31 +0700

    move colour settings to `extend` key

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,23 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      ...defaultTheme.colors,
-      // WargaBantuWarga.com brand colour
-      brand: {
-        DEFAULT: "#1667c2",
-        50: "#c5ddf8",
-        100: "#aed0f6",
-        200: "#80b5f1",
-        300: "#539beb",
-        400: "#2580e6",
-        500: "#1667c2",
-        600: "#114f94",
-        700: "#0c3666",
-        800: "#061e39",
-        900: "#01060b",
-      },
-    },
     extend: {
+      colors: {
+        // WargaBantuWarga.com brand colour
+        brand: {
+          DEFAULT: "#1667c2",
+          50: "#c5ddf8",
+          100: "#aed0f6",
+          200: "#80b5f1",
+          300: "#539beb",
+          400: "#2580e6",
+          500: "#1667c2",
+          600: "#114f94",
+          700: "#0c3666",
+          800: "#061e39",
+          900: "#01060b",
+        },
+      },
       fontFamily: {
         sans: ["Nunito", ...defaultTheme.fontFamily.sans],
       },
