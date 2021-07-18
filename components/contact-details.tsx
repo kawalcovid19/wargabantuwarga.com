6b8e36b5cc5ae466a4ad6d7c454718baa4418870
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-07-18 20:44:52 +0700

    Re-enable `ReportButton`

--- components/contact-details.tsx
+++ components/contact-details.tsx
@@ -9,6 +9,25 @@ type ReportButtonProps = {
   onClick: () => void;
 };
 
+type ReportButtonHandlerProps = {
+  contact: Contact;
+  provinceName: string;
+};
+
+const reportButtonHandler = (props: ReportButtonHandlerProps) => {
+  const {
+    provinceName,
+    contact: { keterangan, penyedia, kontak },
+  } = props;
+
+  const formBaseUrl =
+    "https://docs.google.com/forms/d/e/1FAIpQLSeDdZLMD9RTfOiKMItNb8542KAO5w3x9O2ZFQmNyff1rYRZvQ/viewform?usp=pp_url";
+
+  const formQuery = `&entry.346789668=__other_option__&entry.346789668.other_option_response=${keterangan}&entry.323081545=${provinceName}&entry.68818336=${penyedia}&entry.217416134=${kontak}`;
+
+  return window.open(formBaseUrl.concat(encodeURI(formQuery)), "_blank");
+};
+
 const ReportButton = (props: ReportButtonProps) => (
   <span className="ml-4 flex-shrink-0">
     <button
@@ -24,6 +43,7 @@ const ReportButton = (props: ReportButtonProps) => (
 type DescriptionItemProps = {
   label: string;
   value?: string;
+  onClick: () => void;
 };
 
 const DescriptionItem = (props: DescriptionItemProps) => (
@@ -31,6 +51,7 @@ const DescriptionItem = (props: DescriptionItemProps) => (
     <dt className="text-sm font-medium text-gray-500">{props.label}</dt>
     <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
       <span className="flex-grow">{props.value}</span>
+      <ReportButton onClick={props.onClick} />
     </dd>
   </div>
 );
@@ -39,21 +60,10 @@ type DescriptionLinkProps = {
   label: string;
   value?: string;
   contact: Contact;
-  provinceName: string;
+  onClick: () => void;
 };
 
 const DescriptionLink = (props: DescriptionLinkProps) => {
-  const { keterangan, penyedia, kontak } = props.contact;
-
-  const formBaseUrl =
-    "https://docs.google.com/forms/d/e/1FAIpQLSeDdZLMD9RTfOiKMItNb8542KAO5w3x9O2ZFQmNyff1rYRZvQ/viewform?usp=pp_url";
-
-  const formQuery = `&entry.346789668=__other_option__&entry.346789668.other_option_response=${keterangan}&entry.323081545=${props.provinceName}&entry.68818336=${penyedia}&entry.217416134=${kontak}`;
-
-  const reportButtonHandler = () => {
-    window.open(formBaseUrl.concat(encodeURI(formQuery)), "_blank");
-  };
-
   return props.value ? (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4">
       <dt className="text-sm font-medium text-gray-500">{props.label}</dt>
@@ -66,37 +76,64 @@ const DescriptionLink = (props: DescriptionLinkProps) => {
         >
           {props.value}
         </a>
-        <ReportButton onClick={reportButtonHandler} />
+        <ReportButton onClick={props.onClick} />
       </dd>
     </div>
   ) : null;
 };
 
 export function ContactDetails({ contact, provinceName }: ContactDetailsProps) {
+  const _reportButtonHandler = () => {
+    reportButtonHandler({ contact, provinceName });
+  };
+
   return (
     <div className="bg-white shadow overflow-hidden rounded-md">
       <dl className="divide-y divide-gray-200">
-        <DescriptionItem label="Penyedia" value={contact.penyedia} />
-        <DescriptionItem label="Keterangan" value={contact.keterangan} />
-        <DescriptionItem label="Lokasi" value={contact.lokasi} />
-        <DescriptionItem label="Kontak" value={contact.kontak} />
-        <DescriptionItem label="Alamat" value={contact.alamat} />
+        <DescriptionItem
+          label="Penyedia"
+          onClick={_reportButtonHandler}
+          value={contact.penyedia}
+        />
+        <DescriptionItem
+          label="Keterangan"
+          onClick={_reportButtonHandler}
+          value={contact.keterangan}
+        />
+        <DescriptionItem
+          label="Lokasi"
+          onClick={_reportButtonHandler}
+          value={contact.lokasi}
+        />
+        <DescriptionItem
+          label="Kontak"
+          onClick={_reportButtonHandler}
+          value={contact.kontak}
+        />
+        <DescriptionItem
+          label="Alamat"
+          onClick={_reportButtonHandler}
+          value={contact.alamat}
+        />
         <DescriptionLink
           contact={contact}
           label="Tautan"
-          provinceName={provinceName}
+          onClick={_reportButtonHandler}
           value={contact.tautan}
         />
         <DescriptionItem
           label="Terakhir Update"
+          onClick={_reportButtonHandler}
           value={contact.terakhir_update}
         />
         <DescriptionItem
           label="Bentuk Verifikasi"
+          onClick={_reportButtonHandler}
           value={contact.bentuk_verifikasi}
         />
         <DescriptionItem
           label="Tambahan Informasi"
+          onClick={_reportButtonHandler}
           value={contact.tambahan_informasi}
         />
       </dl>
