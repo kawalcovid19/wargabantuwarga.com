110f7f1683ed8a7be0a21ffdcd0703c45e177635
Resi Respati <resir014@gmail.com>
2021-07-18 23:36:46 +0700

    main -> div

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -8,7 +8,7 @@ export default function FallbackError() {
     <Page>
       <PageContent>
         <div className="pt-16 pb-12 flex flex-col">
-          <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
             <div className="py-16">
               <div className="text-center">
                 <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
@@ -28,7 +28,7 @@ export default function FallbackError() {
                 </div>
               </div>
             </div>
-          </main>
+          </div>
         </div>
       </PageContent>
     </Page>
