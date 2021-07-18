a44e1e220857ad6bd05df7682b61a63ee6c86051
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-07-18 15:45:19 +0700

    get form url

--- components/contact-details.tsx
+++ components/contact-details.tsx
@@ -5,18 +5,22 @@ type ContactDetailsProps = {
   provinceName: string;
 };
 
-/* TODO: Integrate this button with a pre-filled Google Forms URL
-const ReportButton = () => (
+type ReportButtonProps = {
+  onClick: () => void;
+};
+
+//  TODO: Integrate this button with a pre-filled Google Forms URL
+const ReportButton = (props: ReportButtonProps) => (
   <span className="ml-4 flex-shrink-0">
     <button
       className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      onClick={props.onClick}
       type="button"
     >
       Laporkan kesalahan
     </button>
   </span>
 );
-*/
 
 type DescriptionItemProps = {
   label: string;
@@ -35,10 +39,20 @@ const DescriptionItem = (props: DescriptionItemProps) => (
 type DescriptionLinkProps = {
   label: string;
   value?: string;
+  contact: Contact;
 };
 
-const DescriptionLink = (props: DescriptionLinkProps) =>
-  props.value ? (
+const DescriptionLink = (props: DescriptionLinkProps) => {
+  const { lokasi, keterangan, penyedia, kontak } = props.contact;
+  const baseFormUrl =
+    "https://docs.google.com/forms/d/e/1FAIpQLSeDdZLMD9RTfOiKMItNb8542KAO5w3x9O2ZFQmNyff1rYRZvQ/viewform?usp=pp_url";
+  const formValue = `&entry.346789668=${keterangan}&entry.323081545=${lokasi}&entry.68818336=${penyedia}&entry.217416134=${kontak}&entry.11952303=`;
+
+  const reportButtonHandler = () => {
+    window.open(baseFormUrl.concat(encodeURI(formValue)), "_blank");
+  };
+
+  return props.value ? (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4">
       <dt className="text-sm font-medium text-gray-500">{props.label}</dt>
       <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-3">
@@ -50,9 +64,11 @@ const DescriptionLink = (props: DescriptionLinkProps) =>
         >
           {props.value}
         </a>
+        <ReportButton onClick={reportButtonHandler} />
       </dd>
     </div>
   ) : null;
+};
 
 export function ContactDetails({ contact }: ContactDetailsProps) {
   return (
@@ -63,7 +79,11 @@ export function ContactDetails({ contact }: ContactDetailsProps) {
         <DescriptionItem label="Lokasi" value={contact.lokasi} />
         <DescriptionItem label="Kontak" value={contact.kontak} />
         <DescriptionItem label="Alamat" value={contact.alamat} />
-        <DescriptionLink label="Tautan" value={contact.tautan} />
+        <DescriptionLink
+          contact={contact}
+          label="Tautan"
+          value={contact.tautan}
+        />
         <DescriptionItem
           label="Tanggal Verifikasi"
           value={contact.tanggal_verifikasi}
