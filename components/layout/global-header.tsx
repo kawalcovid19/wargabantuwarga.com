66fb49d4cb373dc580e0d85d702624b7f0876318
Resi Respati <resir014@gmail.com>
2021-07-18 11:58:49 +0700

    proper global navigation
    Fixes #122.

--- components/layout/global-header.tsx
+++ components/layout/global-header.tsx
@@ -1,32 +1,7 @@
-import { bannerBlurData, imgixLoader } from "../../lib/imgix-loader";
-
-import Image from "next/image";
-import Link from "next/link";
-
-// TODO: Replace with a more suitable global navbar.
 export function GlobalHeader() {
   return (
-    <header>
-      <div className="max-w-xl mx-auto">
-        <h1 className="p-0">
-          <Link href="/">
-            <a>
-              <Image
-                alt="Warga Bantu Warga"
-                blurDataURL={bannerBlurData}
-                height={287}
-                layout="responsive"
-                loader={imgixLoader}
-                placeholder="blur"
-                priority={true}
-                quality={70}
-                src="hero_banner.png"
-                width={640}
-              />
-            </a>
-          </Link>
-        </h1>
-      </div>
+    <header className="bg-brand-500 z-50 h-24">
+      <h1>WBW</h1>
     </header>
   );
 }
