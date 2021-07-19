33866055bf735a0fdf7feeff46909ed74c9268f8
Eka <6597211+ekafyi@users.noreply.github.com>
2021-07-19 20:15:38 +0700

    add CopyButton to Contact Detail page

--- components/contact-details.tsx
+++ components/contact-details.tsx
@@ -1,6 +1,7 @@
+import { CopyButton } from "../components/copy-button";
 import { anchorTransformer } from "../lib/htmr-transformers";
 import { Contact } from "../lib/provinces";
-import { isNotEmpty } from "../lib/string-utils";
+import { isNotEmpty, stripTags } from "../lib/string-utils";
 
 import htmr from "htmr";
 import { HtmrOptions } from "htmr/src/types";
@@ -48,6 +49,7 @@ const ReportButton = (props: ReportButtonProps) => (
 type DescriptionItemProps = {
   label: string;
   value?: string;
+  withCopyButton?: boolean;
   onClick: () => void;
 };
 
@@ -64,6 +66,9 @@ const DescriptionItem = (props: DescriptionItemProps) => {
         <span className="flex-grow">
           {htmr(value, { transform: htmrTransform })}
         </span>
+        {typeof value == "string" &&
+          value.length > 0 &&
+          props.withCopyButton && <CopyButton text={stripTags(value)} />}
         <ReportButton onClick={props.onClick} />
       </dd>
     </div>
@@ -97,11 +102,13 @@ export function ContactDetails({ contact, provinceName }: ContactDetailsProps) {
           label="Kontak"
           onClick={_reportButtonHandler}
           value={contact.kontak}
+          withCopyButton
         />
         <DescriptionItem
           label="Alamat"
           onClick={_reportButtonHandler}
           value={contact.alamat}
+          withCopyButton
         />
         <DescriptionItem
           label="Tautan"
