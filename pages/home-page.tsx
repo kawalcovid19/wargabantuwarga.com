4f057b96d81df24d58f35eed9299c08ea54437a1
Zain Fathoni <me@zainf.dev>
2021-07-20 09:23:48 +0800

    Add the `Image` header

--- pages/home-page.tsx
+++ pages/home-page.tsx
@@ -2,7 +2,10 @@ import { attributes, html } from "../_content/home-page.md";
 import { Page } from "../components/layout/page";
 import { PageContent } from "../components/layout/page-content";
 import config from "../lib/config";
+import { bannerBlurData, imgixLoader } from "../lib/imgix-loader";
 
+import Image from "next/image";
+import Link from "next/link";
 import { NextSeo } from "next-seo";
 
 const meta = {
@@ -12,6 +15,28 @@ const meta = {
 const HomePage = () => (
   <Page>
     <NextSeo title={meta.title} titleTemplate="%s" />
+    <header>
+      <div className="max-w-xl mx-auto">
+        <h1 className="p-0">
+          <Link href="/">
+            <a>
+              <Image
+                alt="Warga Bantu Warga"
+                blurDataURL={bannerBlurData}
+                height={287}
+                layout="responsive"
+                loader={imgixLoader}
+                placeholder="blur"
+                priority={true}
+                quality={70}
+                src="hero_banner.png"
+                width={640}
+              />
+            </a>
+          </Link>
+        </h1>
+      </div>
+    </header>
     <PageContent>
       <article className="prose prose-indigo">
         <h1>{attributes.title}</h1>
