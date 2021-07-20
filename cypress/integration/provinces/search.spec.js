64c1a33a42782616c82059970c64f2ad56bd7fa4
Gilbert Emerson <emer7@users.noreply.github.com>
2021-07-21 00:40:29 +0800

    Add e2e test for query param scenario

--- cypress/integration/provinces/search.spec.js
+++ cypress/integration/provinces/search.spec.js
@@ -1,4 +1,4 @@
-describe("provinces page search", () => {
+describe("provinces page typed search", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/provinces");
   });
@@ -26,6 +26,37 @@ describe("provinces page search", () => {
 
   it("gives no result when search keyword does not correspond to any provinces in Indonesia", () => {
     cy.get("input#keywordsInput").type("asdf");
+    cy.get("ul").first().children().should("have.length", 0);
+  });
+});
+
+describe("provinces page query param search", () => {
+  it("gives only one result of DKI Jakarta when search keyword is dki", () => {
+    cy.visit("http://localhost:3000/provinces?q=dki");
+
+    cy.get("ul").first().children().should("have.length", 1);
+    cy.get("ul")
+      .first()
+      .children()
+      .first()
+      .should("contains.text", "DKI Jakarta");
+  });
+
+  it("gives 5 result of Sulawesi Utara, Tengah, Selatan, Tenggara, Barat when search keyword is sulawesi", () => {
+    cy.visit("http://localhost:3000/provinces?q=sulawesi");
+
+    cy.get("ul").first().children().should("have.length", 5);
+    cy.get("ul")
+      .first()
+      .children()
+      .each((liElement) => {
+        cy.wrap(liElement).should("contains.text", "Sulawesi");
+      });
+  });
+
+  it("gives no result when search keyword does not correspond to any provinces in Indonesia", () => {
+    cy.visit("http://localhost:3000/provinces?q=asdf");
+
     cy.get("ul").first().children().should("have.length", 0);
   });
 });
