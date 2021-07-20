b0867a1dc8c79327951146184d1f0a17dd9e42ba
Zain Fathoni <me@zainf.dev>
2021-07-20 09:17:33 +0800

    Include NextSeo in the HomePage

--- pages/home-page.tsx
+++ pages/home-page.tsx
@@ -1,9 +1,17 @@
 import { attributes, html } from "../_content/home-page.md";
 import { Page } from "../components/layout/page";
 import { PageContent } from "../components/layout/page-content";
+import config from "../lib/config";
+
+import { NextSeo } from "next-seo";
+
+const meta = {
+  title: `${config.site_name} | ${config.site_tagline}`,
+};
 
 const HomePage = () => (
   <Page>
+    <NextSeo title={meta.title} titleTemplate="%s" />
     <PageContent>
       <article className="prose prose-indigo">
         <h1>{attributes.title}</h1>
