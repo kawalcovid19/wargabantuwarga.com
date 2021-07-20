93ef450e16f2b52e530b63012395d86fff65966f
Resi Respati <resir014@gmail.com>
2021-07-20 16:45:23 +0700

    minor tweaks to form input + button styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,13 +4,16 @@ import * as React from "react";
 
 import clsx from "clsx";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  block?: boolean;
+}
 
 export const PrimaryButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, style, type, children, ...rest }, ref) => (
+  ({ className, style, type, block, children, ...rest }, ref) => (
     <button
       className={clsx(
-        "inline-flex items-center px-4 py-2 text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500",
+        block ? "flex" : "inline-flex",
+        "items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500",
         className,
       )}
       ref={ref}
@@ -26,10 +29,11 @@ export const PrimaryButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
 PrimaryButton.displayName = "PrimaryButton";
 
 export const SecondaryButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, style, type, children, ...rest }, ref) => (
+  ({ className, style, type, children, block, ...rest }, ref) => (
     <button
       className={clsx(
-        "inline-flex items-center px-4 py-2 text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500",
+        block ? "flex" : "inline-flex",
+        "items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500",
         className,
       )}
       ref={ref}
