d50f8c9f091c50f9d134975f2590a6b5dbddd99d
Resi Respati <resir014@gmail.com>
2021-07-21 13:55:19 +0700

    added GhostedButton

--- components/ui/button/button.tsx
+++ components/ui/button/button.tsx
@@ -104,6 +104,51 @@ export const SecondaryButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
 
 SecondaryButton.displayName = "SecondaryButton";
 
+export const GhostedButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      style,
+      type,
+      block,
+      size = "md",
+      rounded,
+      icon,
+      children,
+      isLoading,
+      loadingText = "Memuat...",
+      disabled,
+      ...rest
+    },
+    ref,
+  ) => (
+    <button
+      className={clsx(
+        buttonBlockStyles(block),
+        buttonSizes(size),
+        buttonRoundedStyles(rounded, size),
+        "items-center border border-transparent font-medium text-blue-700 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500",
+        disabledStyles,
+        className,
+      )}
+      disabled={isLoading ?? disabled}
+      ref={ref}
+      style={style}
+      type={type ?? "button"}
+      {...rest}
+    >
+      {renderIcon(
+        isLoading ? Spinner : icon,
+        size,
+        isLoading ? "animate-spin" : undefined,
+      )}
+      {isLoading ? loadingText : children}
+    </button>
+  ),
+);
+
+GhostedButton.displayName = "GhostedButton";
+
 export const WhiteButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
     {
