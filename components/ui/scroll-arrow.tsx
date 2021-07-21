046482795b0e5b6ff3736ca6b74a21f3dbfdf641
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-07-22 00:05:25 +0800

    Create basic implementation of scroll to top arrow component

---
+++ components/ui/scroll-arrow.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+import { ArrowCircleUpIcon } from "@heroicons/react/solid";
+
+export const ScrollArrow = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = useCallback(() => {
+    const heightThreshold = 400;
+
+    if (!showScroll && window.pageYOffset > heightThreshold) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= heightThreshold) {
+      setShowScroll(false);
+    }
+  }, [showScroll]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkScrollTop);
+    return () => window.removeEventListener("scroll", checkScrollTop);
+  }, [checkScrollTop]);
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <ArrowCircleUpIcon
+      className="fixed cursor-pointer bottom-20 right-10"
+      onClick={scrollTop}
+      style={{ height: 40, display: showScroll ? "flex" : "none" }}
+    />
+  );
+};
