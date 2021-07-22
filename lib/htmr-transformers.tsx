8442fd019270f30fa56377c907cacfdbf4992dc2
Zain Fathoni <me@zainf.dev>
2021-07-22 08:10:52 +0800

    Implement headings transformers

--- lib/htmr-transformers.tsx
+++ lib/htmr-transformers.tsx
@@ -1,3 +1,5 @@
+import { getKebabCase } from "./string-utils";
+
 import Link from "next/link";
 
 export const anchorTransformer = (node: JSX.IntrinsicElements["a"]) => {
@@ -30,4 +32,59 @@ export const anchorTransformer = (node: JSX.IntrinsicElements["a"]) => {
       {children}
     </a>
   );
+};
+
+// TODO: Unify these headings transformers into a single generic function
+export const heading1Transformer = (node: JSX.IntrinsicElements["h1"]) => {
+  const { children } = node;
+
+  if (typeof children === "string") {
+    return <h1 id={getKebabCase(children)}>{children}</h1>;
+  }
+  return <h1>children</h1>;
+};
+
+export const heading2Transformer = (node: JSX.IntrinsicElements["h2"]) => {
+  const { children } = node;
+
+  if (typeof children === "string") {
+    return <h2 id={getKebabCase(children)}>{children}</h2>;
+  }
+  return <h2>children</h2>;
+};
+
+export const heading3Transformer = (node: JSX.IntrinsicElements["h3"]) => {
+  const { children } = node;
+
+  if (typeof children === "string") {
+    return <h3 id={getKebabCase(children)}>{children}</h3>;
+  }
+  return <h3>children</h3>;
+};
+
+export const heading4Transformer = (node: JSX.IntrinsicElements["h4"]) => {
+  const { children } = node;
+
+  if (typeof children === "string") {
+    return <h4 id={getKebabCase(children)}>{children}</h4>;
+  }
+  return <h4>children</h4>;
+};
+
+export const heading5Transformer = (node: JSX.IntrinsicElements["h5"]) => {
+  const { children } = node;
+
+  if (typeof children === "string") {
+    return <h5 id={getKebabCase(children)}>{children}</h5>;
+  }
+  return <h5>children</h5>;
+};
+
+export const heading6Transformer = (node: JSX.IntrinsicElements["h6"]) => {
+  const { children } = node;
+
+  if (typeof children === "string") {
+    return <h6 id={getKebabCase(children)}>{children}</h6>;
+  }
+  return <h6>children</h6>;
 };
