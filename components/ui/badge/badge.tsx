60f490f502719ac4272d687defc514a6544cd4e2
Hani Husamuddin <hani.husam@gmail.com>
2021-07-23 21:04:03 +0700

    Create initial component for Badge

--- components/ui/badge/badge.tsx
+++ components/ui/badge/badge.tsx
@@ -1,3 +1,9 @@
 import * as React from "react";
 
-export const Badge = () => <h1>Badge</h1>;
+export const Badge = ({ children }) => {
+  return (
+    <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+      {children}
+    </span>
+  );
+};
