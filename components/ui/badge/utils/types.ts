1933ce97c51e261fa954e89f138d24b497276a1a
Hani Husamuddin <hani.husam@gmail.com>
2021-07-23 21:03:45 +0700

    Create initial types for Badge

---
+++ components/ui/badge/utils/types.ts
@@ -0,0 +1,29 @@
+export type BadgeSizes = "basic" | "large";
+export type BadgeColor =
+  | "blue"
+  | "gray"
+  | "green"
+  | "indigo"
+  | "pink"
+  | "purple"
+  | "red"
+  | "yellow";
+
+interface BadgeBaseProps {
+  /** A string of all className you want applied to the component */
+  className?: string;
+  /** Is the badge rounded? */
+  rounded?: boolean;
+  /** The size of the badge. Available sizes: `"basic" | "large"` */
+  size?: BadgeSizes;
+  /** The color of the badge. Available colors: `"blue" | "gray" | "green" | "indigo" | "pink" | "purple" | "red" | "yellow"` */
+  color?: BadgeColor;
+  /** Component used for the root node. Either a string to use a HTML element or a component
+   *
+   * @default 'span'
+   */
+  component?: React.ElementType | string;
+}
+
+export type BadgeProps = BadgeBaseProps &
+  React.HTMLAttributes<HTMLDivElement | HTMLSpanElement>;
