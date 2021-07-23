079eaffb963e1de63d3545c15f09a0f9ba5adc99
Hani Husamuddin <hani.husam@gmail.com>
2021-07-23 21:20:30 +0700

    add badge size and color helper fn

--- components/ui/badge/utils/helpers.ts
+++ components/ui/badge/utils/helpers.ts
@@ -1,7 +1,55 @@
+import { BadgeColor, BadgeSizes } from "./types";
+
 export function badgeRoundedStyles(rounded?: boolean) {
   if (rounded) {
     return "rounded-md";
   }
 
   return "rounded-full";
+}
+
+export function badgeSizes(size?: BadgeSizes) {
+  switch (size) {
+    case "basic": {
+      return "px-2.5 py-0.5 text-xs";
+    }
+    case "large": {
+      return "px-3 py-0.5 text-sm";
+    }
+    default: {
+      return "px-2.5 py-0.5 text-xs";
+    }
+  }
+}
+
+export function badgeColors(color?: BadgeColor) {
+  switch (color) {
+    case "blue": {
+      return "bg-blue-100 text-blue-800";
+    }
+    case "gray": {
+      return "bg-gray-100 text-gray-800";
+    }
+    case "green": {
+      return "bg-green-100 text-green-800";
+    }
+    case "indigo": {
+      return "bg-indigo-100 text-indigo-800";
+    }
+    case "pink": {
+      return "bg-pink-100 text-pink-800";
+    }
+    case "purple": {
+      return "bg-purple-100 text-purple-800";
+    }
+    case "red": {
+      return "bg-red-100 text-red-800";
+    }
+    case "yellow": {
+      return "bg-yellow-100 text-yellow-800";
+    }
+    default: {
+      return "bg-gray-100 text-gray-800";
+    }
+  }
 }
