2b9663d59b8d99dc5525a928e4be869a489289de
Resi Respati <resir014@gmail.com>
2021-07-23 18:59:29 +0700

    redesign bottom navigation following Website 2.0 design

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 
-import { bottomNavigation } from "~/lib/layout/navigation-data";
+import {
+  bottomNavigation,
+  BottomNavigationItem,
+} from "~/lib/layout/navigation-data";
 
 import clsx from "clsx";
 import Link from "next/link";
@@ -9,52 +12,50 @@ import { useRouter } from "next/router";
 export function Navigation() {
   const router = useRouter();
 
+  const menuClasses = (isActive: boolean) => {
+    return [
+      "inline-flex flex-col items-center justify-center h-12 rounded-md",
+      isActive ? "text-blue-600" : "text-gray-600",
+      "hover:text-blue-700",
+    ];
+  };
+
+  const renderItem = (item: BottomNavigationItem) => {
+    return (
+      <>
+        {React.createElement(item.icon, {
+          className: "w-8 h-8",
+          "aria-hidden": true,
+        })}
+        <span className="text-xs">{item.name}</span>
+      </>
+    );
+  };
+
   return (
     <nav className="flex items-center justify-center fixed bottom-0 w-full h-16 px-4 bg-white border-t border-gray-300 z-40">
       <div className="flex items-center justify-center w-full max-w-xl mx-auto">
-        <ul className="flex items-center space-x-4">
+        <ul className="flex items-center justify-evenly w-full">
           {bottomNavigation.map((item) => {
             const isActive = item.exact
               ? item.href === router.asPath
               : router.asPath.startsWith(item.href);
 
             return (
-              <li key={item.name} className="font-sans">
+              <li key={item.name} className="relative">
                 {item.external ? (
                   <a
-                    className={clsx(
-                      "inline-flex items-center justify-center h-12 w-12 rounded-md",
-                      isActive
-                        ? "bg-blue-100 text-blue-600 hover:text-blue-700"
-                        : "text-gray-600 hover:text-gray-700",
-                      "hover:bg-blue-100",
-                    )}
+                    className={clsx(...menuClasses(isActive))}
                     href={item.href}
                     rel="noreferrer"
                     target="_blank"
                   >
-                    {React.createElement(item.icon, {
-                      className: "w-8 h-8",
-                      "aria-hidden": true,
-                    })}
-                    <span className="sr-only">{item.name}</span>
+                    {renderItem(item)}
                   </a>
                 ) : (
                   <Link href={item.href}>
-                    <a
-                      className={clsx(
-                        "inline-flex items-center justify-center h-12 w-12 rounded-md",
-                        isActive
-                          ? "bg-blue-100 text-blue-600 hover:text-blue-700"
-                          : "text-gray-600 hover:text-gray-700",
-                        "hover:bg-blue-100",
-                      )}
-                    >
-                      {React.createElement(item.icon, {
-                        className: "w-8 h-8",
-                        "aria-hidden": true,
-                      })}
-                      <span className="sr-only">{item.name}</span>
+                    <a className={clsx(...menuClasses(isActive))}>
+                      {renderItem(item)}
                     </a>
                   </Link>
                 )}
