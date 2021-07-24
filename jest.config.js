e07ce6a40f3802235e4ed07d39f14f60d4420e98
Zain Fathoni <me@zainf.dev>
2021-07-24 18:25:20 +0800

    Configure jest-dom to extend the expect() assertions

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleNameMapper: {
     "~/(.*)": "<rootDir>/$1",
   },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   testEnvironment: "jest-environment-jsdom",
   transform: {
     "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
