aa046d1653ea5b73322694d013597170059fe437
Zain Fathoni <me@zainf.dev>
2021-07-25 12:02:15 +0800

    Test `FaqPage`

---
+++ pages/__tests__/faq.test.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { faqBuilder } from "~/lib/__mocks__/builders/faq";
+import faqSheets from "~/lib/faq-databases";
+
+import FaqPage, { getStaticProps } from "../faq";
+
+import { render, screen } from "@testing-library/react";
+
+describe("FaqPage", () => {
+  const faq = faqBuilder();
+  const faqs = [faq];
+
+  it("renders the title correctly", () => {
+    render(<FaqPage faqSheets={faqs} />);
+
+    expect(screen.getByText(/pertanyaan yang sering ditanyakan/i))
+      .toMatchInlineSnapshot(`
+      <h2
+        class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate"
+      >
+        Pertanyaan yang sering ditanyakan
+      </h2>
+    `);
+  });
+
+  it("renders the questions and answers correctly", () => {
+    render(<FaqPage faqSheets={faqs} />);
+
+    expect(screen.getByText(faq.pertanyaan)).toBeVisible();
+    expect(screen.getByText(faq.jawaban)).toBeVisible();
+  });
+  it("renders the links correctly", () => {
+    render(<FaqPage faqSheets={faqs} />);
+
+    const link = screen.getByText(faq.sumber as string);
+
+    expect(screen.getByText(/sumber:/i)).toBeVisible();
+    expect(link).toBeVisible();
+    expect(link).toHaveAttribute("href", faq.link);
+  });
+});
+
+describe("getStaticProps", () => {
+  it("returns the props from the faq-sheets correctly", () => {
+    expect(getStaticProps({})).toEqual({
+      props: { faqSheets },
+    });
+  });
+});
