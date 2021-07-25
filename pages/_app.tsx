a6be07e67173665521f1a255d484f9d7e7104e5c
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-07-25 23:15:46 +0700

    fix: outide next/head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -82,25 +82,25 @@ export default function App({ Component, pageProps, router }: AppProps) {
         <link href="/favicon.ico" rel="icon" />
         <link href="/manifest.json" rel="manifest" />
         <meta content="#1667C2" name="theme-color" />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+      </Head>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5X4ZPBX');`,
-          }}
+        }}
+      />
+      <noscript>
+        <iframe
+          height="0"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5X4ZPBX"
+          style={{ display: "none", visibility: "hidden" }}
+          title="gtm"
+          width="0"
         />
-        <noscript>
-          <iframe
-            height="0"
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5X4ZPBX"
-            style={{ display: "none", visibility: "hidden" }}
-            title="gtm"
-            width="0"
-          />
-        </noscript>
-      </Head>
+      </noscript>
       <Component {...pageProps} />
     </LayoutRoot>
   );
