b0991afc7c6edf1d842a55e9791010697a2a8842
Zain Fathoni <me@zainf.dev>
2021-07-25 08:09:34 +0800

    Memoize filterForms and sortForms

--- components/search-filter-modal.tsx
+++ components/search-filter-modal.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { ChangeEventHandler, Fragment, useRef } from "react";
+import { ChangeEventHandler, Fragment, useMemo, useRef } from "react";
 
 import { FormLabel } from "./ui/forms/form-label";
 import { InputSelect } from "./ui/forms/input-select";
@@ -43,6 +43,87 @@ export function SearchFilterModal({
 }: SearchFilterModalProps) {
   const closeButtonRef = useRef(null);
 
+  const filterForms = useMemo(
+    () =>
+      filterItems && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          {Object.entries(filterItems).map(([key, value], idx) => {
+            const { title, buckets }: any = value;
+            return (
+              <div key={`filter-${idx}`} className="space-y-1">
+                <FormLabel htmlFor={`filter-${key}`}>{title}</FormLabel>
+                <InputSelect
+                  name={key}
+                  onChange={handleFilterChange}
+                  title={title}
+                  value={filters?.[key]?.length ? filters[key][0] : ""}
+                >
+                  <option value="">Semua</option>
+                  {buckets.map((bucket: any, bIdx: number) => {
+                    if (bucket.key) {
+                      if (checkDocSize) {
+                        if (bucket.doc_count > 0) {
+                          return (
+                            <option
+                              key={`option-${key}-${bIdx + 1}`}
+                              value={bucket.key}
+                            >
+                              {bucket.key}
+                            </option>
+                          );
+                        }
+                        // Do not print, when doc_count = 0 and checkDocSize set to true
+                        return null;
+                      } else {
+                        // FAQ page doesn't check the doc_count
+                        // just pass checkDocSize props to false
+                        return (
+                          <option
+                            key={`option-${key}-${bIdx + 1}`}
+                            value={bucket.key}
+                          >
+                            {bucket.key}
+                          </option>
+                        );
+                      }
+                    }
+                    return null;
+                  })}
+                </InputSelect>
+              </div>
+            );
+          })}
+        </div>
+      ),
+    [filterItems, handleFilterChange, filters, checkDocSize],
+  );
+
+  const sortForms = useMemo(
+    () =>
+      sortSettings?.length && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="space-y-1">
+            <FormLabel htmlFor="sort-by">Urut berdasarkan</FormLabel>
+            <InputSelect
+              name="sort-by"
+              onChange={handleSortChange}
+              title="Urut berdasarkan"
+              value={sortBy}
+            >
+              {sortSettings.map((cur, idx) => {
+                return (
+                  <option key={`sort-by-${idx}`} value={cur.value}>
+                    {cur.label}
+                  </option>
+                );
+              })}
+            </InputSelect>
+          </div>
+        </div>
+      ),
+    [handleSortChange, sortBy, sortSettings],
+  );
+
   const renderFilterForms = () => {
     if (isLoading) {
       return (
@@ -60,77 +141,8 @@ export function SearchFilterModal({
 
     return (
       <>
-        {filterItems && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            {Object.entries(filterItems).map(([key, value], idx) => {
-              const { title, buckets }: any = value;
-              return (
-                <div key={`filter-${idx}`} className="space-y-1">
-                  <FormLabel htmlFor={`filter-${key}`}>{title}</FormLabel>
-                  <InputSelect
-                    name={key}
-                    onChange={handleFilterChange}
-                    title={title}
-                    value={filters?.[key]?.length ? filters[key][0] : ""}
-                  >
-                    <option value="">Semua</option>
-                    {buckets.map((bucket: any, bIdx: number) => {
-                      if (bucket.key) {
-                        if (checkDocSize) {
-                          if (bucket.doc_count > 0) {
-                            return (
-                              <option
-                                key={`option-${key}-${bIdx + 1}`}
-                                value={bucket.key}
-                              >
-                                {bucket.key}
-                              </option>
-                            );
-                          }
-                          // Do not print, when doc_count = 0 and checkDocSize set to true
-                          return null;
-                        } else {
-                          // FAQ page doesn't check the doc_count
-                          // just pass checkDocSize props to false
-                          return (
-                            <option
-                              key={`option-${key}-${bIdx + 1}`}
-                              value={bucket.key}
-                            >
-                              {bucket.key}
-                            </option>
-                          );
-                        }
-                      }
-                      return null;
-                    })}
-                  </InputSelect>
-                </div>
-              );
-            })}
-          </div>
-        )}
-        {sortSettings?.length && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <div className="space-y-1">
-              <FormLabel htmlFor="sort-by">Urut berdasarkan</FormLabel>
-              <InputSelect
-                name="sort-by"
-                onChange={handleSortChange}
-                title="Urut berdasarkan"
-                value={sortBy}
-              >
-                {sortSettings.map((cur, idx) => {
-                  return (
-                    <option key={`sort-by-${idx}`} value={cur.value}>
-                      {cur.label}
-                    </option>
-                  );
-                })}
-              </InputSelect>
-            </div>
-          </div>
-        )}
+        {filterForms}
+        {sortForms}
       </>
     );
   };
