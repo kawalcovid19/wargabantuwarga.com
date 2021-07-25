01738eccbef09e461f37d9079a51ba7be5e2cd44
Resi Respati <resir014@gmail.com>
2021-07-26 02:10:06 +0700

    refactor: move homepage menu to a separate component

--- components/home/homepage-start.tsx
+++ components/home/homepage-start.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 import { cloudinaryLoader } from "~/lib/cloudinary-loader";
-import { homepageMenus } from "~/lib/home/homepage-menus";
 
 import { PrimaryAnchorButton } from "../ui/button/primary-anchor-button";
+
+import { HomePageMenu } from "./homepage-menu";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -29,28 +30,7 @@ export function HomePageStart() {
         </Link>
       </div>
       <div className="px-4 py-6">
-        <ul className="grid grid-cols-2 gap-3 sm:gap-6 sm:grid-cols-4">
-          {homepageMenus.map((item) => (
-            <li
-              key={item.name}
-              className="inline-flex flex-col items-center justify-center text-center relative shadow-lg rounded-lg py-3 px-2"
-            >
-              <div
-                aria-hidden
-                className="flex items-center justify-center h-10 w-10 bg-blue-100 rounded-full"
-              >
-                {React.createElement(item.icon, {
-                  className: "h-6 w-6 text-brand-500",
-                })}
-              </div>
-              <Link href={item.href}>
-                <a className="text-gray-900 font-semibold text-xs mt-3 helper-link-cover">
-                  {item.name}
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <HomePageMenu />
       </div>
       <div className="px-4 py-6">
         <a
