4ba40c0b612dd559f7ef73facde802dd8389c762
Zain Fathoni <me@zainf.dev>
2021-07-25 08:35:47 +0800

    Memoize ContactListItem

--- components/contact-list.tsx
+++ components/contact-list.tsx
@@ -1,27 +1,12 @@
 import { useMemo } from "react";
 
-import { CopyButton } from "~/components/copy-button";
 import { EmptyState } from "~/components/ui/empty-state";
 import { ContactListSkeleton } from "~/components/ui/skeleton-loading";
-import { anchorTransformer } from "~/lib/htmr-transformers";
-import { getContactMetaTitle } from "~/lib/meta";
 import { Contact } from "~/lib/provinces";
-import { isNotEmpty, stripTags } from "~/lib/string-utils";
 
-import { Badge } from "./ui/badge";
+import { ContactListItem } from "./contact-list-item";
 
-import {
-  BadgeCheckIcon as BadgeCheckIconUnverified,
-  ExclamationCircleIcon,
-} from "@heroicons/react/outline";
-import {
-  BadgeCheckIcon,
-  LocationMarkerIcon,
-  PhoneIcon,
-} from "@heroicons/react/solid";
-import htmr from "htmr";
-import { HtmrOptions } from "htmr/src/types";
-import Link from "next/link";
+import { ExclamationCircleIcon } from "@heroicons/react/outline";
 
 type ContactListProps = {
   data: Contact[];
@@ -31,101 +16,23 @@ type ContactListProps = {
 };
 
 export function ContactList(props: ContactListProps) {
-  const contactList = useMemo(() => {
-    const htmrTransform: HtmrOptions["transform"] = {
-      a: anchorTransformer,
-    };
-    return (
+  const contactList = useMemo(
+    () => (
       <div className="bg-white shadow overflow-hidden sm:rounded-md">
         <ul className="divide-y divide-gray-200">
           {props.data.map((contact, index) => (
-            <li key={index}>
-              <div className="px-4 py-4 sm:px-6 relative hover:bg-gray-50">
-                <div className="flex items-center justify-between">
-                  <Link
-                    href={`/provinces/${props.provinceSlug}/${contact.slug}`}
-                  >
-                    <a
-                      className="text-sm font-semibold text-blue-600 truncate block helper-link-cover"
-                      title={getContactMetaTitle(props.provinceName, contact)}
-                    >
-                      {isNotEmpty(contact.penyedia)
-                        ? contact.penyedia
-                        : contact.keterangan}
-                    </a>
-                  </Link>
-                  <div className="ml-2 flex-shrink-0 flex">
-                    <Badge color="yellow">{contact.kebutuhan}</Badge>
-                  </div>
-                </div>
-                <div className="mt-2 sm:flex sm:justify-between">
-                  <p className="text-sm font-medium text-gray-600 truncate">
-                    {contact.keterangan}
-                  </p>
-                  {isNotEmpty(contact.terakhir_update) ? (
-                    <div className="mt-2 mb-3 flex items-center text-xs text-gray-500 sm:my-0">
-                      <BadgeCheckIcon
-                        aria-hidden="true"
-                        className="flex-shrink-0 h-4 w-4 sm:order-2 text-green-400"
-                      />
-                      <p className="ml-2 mr-1">
-                        Terverifikasi{" "}
-                        {contact.terakhir_update && (
-                          <time dateTime={contact.terakhir_update}>
-                            {contact.terakhir_update}
-                          </time>
-                        )}
-                      </p>
-                    </div>
-                  ) : (
-                    <div className="mt-2 mb-3 flex items-center text-xs text-gray-400 sm:my-0">
-                      <BadgeCheckIconUnverified
-                        aria-hidden="true"
-                        className="flex-shrink-0 h-4 w-4 sm:order-2 text-gray-400"
-                      />
-                      <p className="ml-2 mr-1">Belum terverifkasi</p>
-                    </div>
-                  )}
-                </div>
-                {isNotEmpty(contact.kontak) && (
-                  <div className="mt-2 flex justify-between w-full">
-                    <p className="flex items-center text-sm text-gray-500">
-                      <PhoneIcon
-                        aria-hidden="true"
-                        className="flex-shrink-0 mr-2 h-4 w-4 text-gray-400"
-                      />
-                      {htmr(contact.kontak as string, {
-                        transform: htmrTransform,
-                      })}
-                    </p>
-                    {typeof contact.kontak == "string" && (
-                      <CopyButton text={stripTags(contact.kontak)} />
-                    )}
-                  </div>
-                )}
-                {isNotEmpty(contact.alamat) && (
-                  <div className="mt-2 flex justify-between w-full">
-                    <p className="mt-2 flex items-start text-sm text-gray-500 sm:mt-0">
-                      <LocationMarkerIcon
-                        aria-hidden="true"
-                        className="flex-shrink-0 mr-2 h-4 w-4 text-gray-400"
-                      />
-                      {htmr(contact.alamat as string, {
-                        transform: htmrTransform,
-                      })}
-                    </p>
-                    {typeof contact.alamat == "string" && (
-                      <CopyButton text={stripTags(contact.alamat)} />
-                    )}
-                  </div>
-                )}
-              </div>
-            </li>
+            <ContactListItem
+              key={index}
+              contact={contact}
+              provinceName={props.provinceName}
+              provinceSlug={props.provinceSlug}
+            />
           ))}
         </ul>
       </div>
-    );
-  }, [props.data, props.provinceName, props.provinceSlug]);
+    ),
+    [props.data, props.provinceName, props.provinceSlug],
+  );
 
   if (props.isLoading) {
     return <ContactListSkeleton />;
