5cbfaabe218763ca613b44fea242f6ccee6fa8f5
Zain Fathoni <me@zainf.dev>
2021-07-26 15:07:48 +0800

    test: test getStaticPaths to bump the code coverage

--- __tests__/pages/provinces/[provinceSlug]/index.test.tsx
+++ __tests__/pages/provinces/[provinceSlug]/index.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import provinces from "~/lib/provinces";
-import ProvincePage from "~/pages/provinces/[provinceSlug]";
+import ProvincePage, { getStaticPaths } from "~/pages/provinces/[provinceSlug]";
 
 import { render, screen } from "@testing-library/react";
 
@@ -24,5 +24,22 @@ describe("ProvincePage", () => {
     expect(breadcrumbs).toBeVisible();
     expect(breadcrumbs).toHaveAttribute("href", `/provinces/${province.slug}`);
     expect(title).toBeVisible();
+  });
+});
+
+describe("getStaticPaths", () => {
+  // TODO: Do a better test because this test is merely a copy of the implementation
+  it("returns the correct paths", () => {
+    const paths = provinces.map((province) => {
+      const provinceSlug = province.slug;
+
+      return {
+        params: { provinceSlug },
+      };
+    });
+    expect(getStaticPaths({})).toEqual({
+      fallback: false,
+      paths,
+    });
   });
 });
--- lib/__mocks__/provinces.ts
+++ lib/__mocks__/provinces.ts
@@ -1,5 +1,19 @@
+import { Province, ProvincePath } from "../provinces";
+
 import { provinceBuilder } from "./builders/provinces";
 
 const provinces = [provinceBuilder(), provinceBuilder()];
+
+export const getProvincesPaths = (): ProvincePath[] => {
+  const provincees = provinces as unknown as Province[];
+
+  return provincees.map((province) => {
+    const provinceSlug = province.slug;
+
+    return {
+      params: { provinceSlug },
+    };
+  });
+};
 
 export default provinces;
