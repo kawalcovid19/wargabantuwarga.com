124dffd5d8714ac5abfa9abb6d766e986b67c7f9
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-07-26 11:35:28 +0700

    chore(gtm): split Script and noscript

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,3 @@
-import { Script } from "~/components/script";
-
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
@@ -11,15 +9,6 @@ export default class MyDocument extends Document {
           <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
         </Head>
         <body>
-          <Script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5X4ZPBX');`,
-            }}
-          />
           <noscript>
             <iframe
               height="0"
