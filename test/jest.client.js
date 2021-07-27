e21584ad2f23ee6574234011f9d82e0ae5e346e0
Resi Respati <resir014@gmail.com>
2021-07-27 16:12:57 +0700

    test: fixed tests regex for components folder

--- test/jest.client.js
+++ test/jest.client.js
@@ -4,7 +4,7 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   testMatch: [
-    "**/components/**/__tests__/*.test.(ts|tsx|js|jsx)",
+    "**/components/**/__tests__/**/*.test.(ts|tsx|js|jsx)",
     "**/lib/__tests__/gtm.test.ts",
     "**/__tests__/pages/**/*.test.(ts|tsx|js|jsx)",
   ],
