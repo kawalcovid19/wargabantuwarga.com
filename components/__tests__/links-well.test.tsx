25ff866b62411810e5d8243351152c6baa9ed98f
Eka <6597211+ekafyi@users.noreply.github.com>
2021-07-27 23:36:32 +0700

    test: prepare test file

---
+++ components/__tests__/links-well.test.tsx
@@ -0,0 +1,31 @@
+// import React from "react";
+
+// import type { LinksWellProps } from "~/components/links-well";
+// import { LinksWell } from "~/components/links-well";
+
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+
+describe("LinksWell", () => {
+  it("renders a title and at least one link", () => {
+    // TODO
+  });
+
+  it("does not render incomplete data", () => {
+    // TODO
+  });
+
+  it("renders external links correctly", () => {
+    // TODO
+  });
+
+  it("renders internal links correctly", () => {
+    // TODO
+  });
+
+  it("renders phone links correctly", () => {
+    // TODO
+  });
+});
+
+export {};
