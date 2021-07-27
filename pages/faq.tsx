43dd6a9c15cffb06a4580df9c2fccd57d093f3d5
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-07-27 21:03:11 +0700

    feat(FAQ): add empty state

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -5,9 +5,11 @@ import { Page } from "~/components/layout/page";
 import { PageContent } from "~/components/layout/page-content";
 import { PageHeader } from "~/components/layout/page-header";
 import { SearchForm } from "~/components/search-form";
+import { EmptyState } from "~/components/ui/empty-state";
 import faqSheets, { Faq } from "~/lib/faqs";
 import { useSearch } from "~/lib/hooks/use-search";
 
+import { ExclamationCircleIcon } from "@heroicons/react/solid";
 import htmr from "htmr";
 import { GetStaticProps } from "next";
 import { NextSeo } from "next-seo";
@@ -50,7 +52,7 @@ export default function FaqPage(props: FaqPageProps) {
     );
   }, [filteredQuestions]);
 
-  console.log(listFaqs);
+  const listFaqsKeys = Object.keys(listFaqs as Record<string, unknown>);
 
   return (
     <Page>
@@ -76,59 +78,66 @@ export default function FaqPage(props: FaqPageProps) {
         />
 
         <div className="space-y-4">
-          {Object.keys(listFaqs as Record<string, unknown>).map(
-            (category: string) => (
-              <div
-                key={category}
-                className="p-4 bg-white shadow overflow-hidden rounded-md"
-              >
-                <div className="relative">
-                  <div
-                    aria-hidden="true"
-                    className="absolute inset-0 flex items-center"
-                  >
-                    <div className="w-full border-t border-gray-300" />
-                  </div>
-                  <div className="relative flex flex-row items-center justify-start">
-                    <span className="pr-3 bg-white text-lg font-medium text-gray-900">
-                      {category}
-                    </span>
-                  </div>
+          {listFaqsKeys.map((category: string) => (
+            <div
+              key={category}
+              className="p-4 bg-white shadow overflow-hidden rounded-md"
+            >
+              <div className="relative">
+                <div
+                  aria-hidden="true"
+                  className="absolute inset-0 flex items-center"
+                >
+                  <div className="w-full border-t border-gray-300" />
                 </div>
-                <dl className="divide-y divide-gray-200">
-                  {listFaqs[category].map((question: Faq) => (
-                    <div
-                      key={question.pertanyaan}
-                      className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8"
-                    >
-                      <dt className="text-base font-semibold text-gray-900 md:col-span-5">
-                        {question.pertanyaan}
-                      </dt>
-                      <dd className="mt-2 md:mt-0 md:col-span-7">
-                        <p className="text-base text-gray-500">
-                          {htmr(question.jawaban)}
-                        </p>
-                        <small>
-                          Sumber:{" "}
-                          {question.link ? (
-                            <a
-                              className="underline text-blue-800"
-                              href={question.link}
-                              rel="noreferrer"
-                              target="_blank"
-                            >
-                              {question.sumber}
-                            </a>
-                          ) : (
-                            question.sumber
-                          )}
-                        </small>
-                      </dd>
-                    </div>
-                  ))}
-                </dl>
+                <div className="relative flex flex-row items-center justify-start">
+                  <span className="pr-3 bg-white text-lg font-medium text-gray-900">
+                    {category}
+                  </span>
+                </div>
               </div>
-            ),
+              <dl className="divide-y divide-gray-200">
+                {listFaqs[category].map((question: Faq) => (
+                  <div
+                    key={question.pertanyaan}
+                    className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8"
+                  >
+                    <dt className="text-base font-semibold text-gray-900 md:col-span-5">
+                      {question.pertanyaan}
+                    </dt>
+                    <dd className="mt-2 md:mt-0 md:col-span-7">
+                      <p className="text-base text-gray-500">
+                        {htmr(question.jawaban)}
+                      </p>
+                      <small>
+                        Sumber:{" "}
+                        {question.link ? (
+                          <a
+                            className="underline text-blue-800"
+                            href={question.link}
+                            rel="noreferrer"
+                            target="_blank"
+                          >
+                            {question.sumber}
+                          </a>
+                        ) : (
+                          question.sumber
+                        )}
+                      </small>
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          ))}
+          {listFaqsKeys.length === 0 && (
+            <div className="px-4">
+              <EmptyState
+                description="Silakan gunakan kata kunci pencarian lainnya."
+                icon={ExclamationCircleIcon}
+                title="Pertanyaan tidak ditemukan"
+              />
+            </div>
           )}
         </div>
       </PageContent>
