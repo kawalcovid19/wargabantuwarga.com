f8500f4d39dea951f0c57662abd154c8217a4198
Zain Fathoni <me@zainf.dev>
2021-07-28 13:56:35 +0800

    test: alertColors

--- components/ui/alert/utils/__tests__/helpers.test.ts
+++ components/ui/alert/utils/__tests__/helpers.test.ts
@@ -1,6 +1,6 @@
 import { Colors } from "~/components/ui/types";
 
-import { accentBorderColors } from "../helpers";
+import { accentBorderColors, alertColors } from "../helpers";
 
 describe("accentBorderColors", () => {
   it.each`
@@ -23,5 +23,25 @@ describe("accentBorderColors", () => {
 
   it(`returns nothing when accentBorder is false`, () => {
     expect(accentBorderColors(false)).toBeUndefined();
+  });
+});
+
+describe("alertColors", () => {
+  it.each`
+    color       | expected
+    ${"blue"}   | ${"bg-blue-50 text-blue-700"}
+    ${"gray"}   | ${"bg-gray-50 text-gray-700"}
+    ${"green"}  | ${"bg-green-50 text-green-700"}
+    ${"indigo"} | ${"bg-indigo-50 text-indigo-700"}
+    ${"pink"}   | ${"bg-pink-50 text-pink-700"}
+    ${"purple"} | ${"bg-purple-50 text-purple-700"}
+    ${"red"}    | ${"bg-red-50 text-red-700"}
+    ${"yellow"} | ${"bg-yellow-50 text-yellow-700"}
+  `("returns $expected classes when color is $color", ({ color, expected }) => {
+    expect(alertColors(color as Colors)).toEqual(expected);
+  });
+
+  it(`returns gray color when color is not defined`, () => {
+    expect(alertColors()).toEqual("bg-gray-50 text-gray-700");
   });
 });
