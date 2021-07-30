76762533d2e3c27c136a9787bf0f7e117dfb0880
Hani Husamuddin <hani.husam@gmail.com>
2021-07-30 22:33:27 +0700

    test(ui): fix helpers test (alert & badge)

--- components/ui/alert/utils/__tests__/helpers.test.ts
+++ components/ui/alert/utils/__tests__/helpers.test.ts
@@ -1,20 +1,21 @@
-import { Colors } from "~/components/ui/types";
+import { BaseColors } from "~/components/ui/types";
 
 import { accentBorderColors, alertColors } from "../helpers";
 
 describe("accentBorderColors", () => {
   it.each`
-    color       | expected
-    ${"blue"}   | ${"border-l-4 border-blue-400"}
-    ${"gray"}   | ${"border-l-4 border-gray-400"}
-    ${"green"}  | ${"border-l-4 border-green-400"}
-    ${"indigo"} | ${"border-l-4 border-indigo-400"}
-    ${"pink"}   | ${"border-l-4 border-pink-400"}
-    ${"purple"} | ${"border-l-4 border-purple-400"}
-    ${"red"}    | ${"border-l-4 border-red-400"}
-    ${"yellow"} | ${"border-l-4 border-yellow-400"}
+    color           | expected
+    ${"blue"}       | ${"border-l-4 border-blue-400"}
+    ${"gray"}       | ${"border-l-4 border-gray-400"}
+    ${"green"}      | ${"border-l-4 border-green-400"}
+    ${"indigo"}     | ${"border-l-4 border-indigo-400"}
+    ${"light-blue"} | ${"border-l-4 border-light-blue-400"}
+    ${"pink"}       | ${"border-l-4 border-pink-400"}
+    ${"purple"}     | ${"border-l-4 border-purple-400"}
+    ${"red"}        | ${"border-l-4 border-red-400"}
+    ${"yellow"}     | ${"border-l-4 border-yellow-400"}
   `("returns $expected classes when color is $color", ({ color, expected }) => {
-    expect(accentBorderColors(true, color as Colors)).toEqual(expected);
+    expect(accentBorderColors(true, color as BaseColors)).toEqual(expected);
   });
 
   it(`returns yellow color when color is not defined`, () => {
@@ -28,20 +29,21 @@ describe("accentBorderColors", () => {
 
 describe("alertColors", () => {
   it.each`
-    color       | expected
-    ${"blue"}   | ${"bg-blue-50 text-blue-700"}
-    ${"gray"}   | ${"bg-gray-50 text-gray-700"}
-    ${"green"}  | ${"bg-green-50 text-green-700"}
-    ${"indigo"} | ${"bg-indigo-50 text-indigo-700"}
-    ${"pink"}   | ${"bg-pink-50 text-pink-700"}
-    ${"purple"} | ${"bg-purple-50 text-purple-700"}
-    ${"red"}    | ${"bg-red-50 text-red-700"}
-    ${"yellow"} | ${"bg-yellow-50 text-yellow-700"}
+    color           | expected
+    ${"blue"}       | ${"bg-blue-50 text-blue-700"}
+    ${"gray"}       | ${"bg-gray-50 text-gray-700"}
+    ${"green"}      | ${"bg-green-50 text-green-700"}
+    ${"indigo"}     | ${"bg-indigo-50 text-indigo-700"}
+    ${"light-blue"} | ${"bg-light-blue-50 text-light-blue-700"}
+    ${"pink"}       | ${"bg-pink-50 text-pink-700"}
+    ${"purple"}     | ${"bg-purple-50 text-purple-700"}
+    ${"red"}        | ${"bg-red-50 text-red-700"}
+    ${"yellow"}     | ${"bg-yellow-50 text-yellow-700"}
   `("returns $expected classes when color is $color", ({ color, expected }) => {
-    expect(alertColors(color as Colors)).toEqual(expected);
+    expect(alertColors(color as BaseColors)).toEqual(expected);
   });
 
-  it(`returns gray color when color is not defined`, () => {
-    expect(alertColors()).toEqual("bg-gray-50 text-gray-700");
+  it(`returns yellow color when color is not defined`, () => {
+    expect(alertColors()).toEqual("bg-yellow-50 text-yellow-700");
   });
 });
