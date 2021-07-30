27a7dc8748c22cfb67ac9535725130c57a96a422
Resi Respati <resir014@gmail.com>
2021-07-30 18:08:56 +0700

    feat: initial latest news section implementation

---
+++ components/home/homepage-latest-news.tsx
@@ -0,0 +1,51 @@
+import { latestNews } from "~/lib/home/latest-news";
+
+import { OutlineAnchorButton } from "../ui/button";
+
+import { HomePageSection } from "./homepage-section";
+
+import { ExternalLinkIcon } from "@heroicons/react/outline";
+import htmr from "htmr";
+
+export function HomePageLatestNews() {
+  return (
+    <HomePageSection className="px-4 py-6 space-y-4">
+      <h2 className="text-lg sm:text-xl font-semibold">Informasi Terbaru</h2>
+      {latestNews.map(({ attributes, html }) => (
+        <article
+          key={attributes.date.toISOString()}
+          className="border border-gray-200 rounded-md p-4 space-y-4"
+        >
+          <div className="space-y-2">
+            <div className="flex flex-row">
+              <h3 className="flex-1 text-base font-semibold text-gray-700 truncate">
+                {attributes.source}
+              </h3>
+              <span className="inline-block flex-none text-gray-400 ml-4">
+                {new Intl.DateTimeFormat("id-ID", {
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric",
+                }).format(attributes.date)}
+              </span>
+            </div>
+            <div className="text-gray-600">{htmr(html)}</div>
+          </div>
+          {attributes.link && (
+            <OutlineAnchorButton
+              block
+              color="light-blue"
+              href={attributes.link}
+              icon={ExternalLinkIcon}
+              iconPosition="right"
+              rel="nofollow noopener noreferrer"
+              target="_blank"
+            >
+              {attributes.link_text}
+            </OutlineAnchorButton>
+          )}
+        </article>
+      ))}
+    </HomePageSection>
+  );
+}
