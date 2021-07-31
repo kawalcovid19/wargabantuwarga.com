e0db6e7d8a9dee24114028cbf5381f9fe8f345cb
Zain Fathoni <me@zainf.dev>
2021-08-01 04:25:34 +0800

    test: gets latestNews props from the markdown files correctly

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { latestNewsItemBuilder } from "~/lib/content/__mocks__/builders/informasi-terbaru";
-import HomePage from "~/pages";
+import HomePage, { getStaticProps } from "~/pages";
 
 import { render, screen } from "@testing-library/react";
 
@@ -17,5 +17,16 @@ describe("HomePage", () => {
         /Pembaruan terakhir pada Selasa, 27 Juli 2021 17.43 WIB/i,
       ),
     ).toBeVisible();
+  });
+});
+
+describe("getStaticProps", () => {
+  it("gets latestNews props from the markdown files correctly", async () => {
+    expect(await getStaticProps({})).toEqual({
+      props: {
+        // TODO: (ZF) Mock the file system to simulate the markdown file
+        latestNews: undefined,
+      },
+    });
   });
 });
