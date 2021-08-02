d14e922b223b7cfa2e92d2a637a2480393aceb9e
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-08-02 10:56:12 +0700

    chore(eslint): remove simple-sort plugin

--- components/search-form.tsx
+++ components/search-form.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable simple-import-sort/imports */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
