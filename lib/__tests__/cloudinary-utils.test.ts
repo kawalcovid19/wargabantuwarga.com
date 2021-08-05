42344b079a0e7845f810c46b380df37fadb5b3fd
Eka <6597211+ekafyi@users.noreply.github.com>
2021-08-05 14:17:50 +0700

    feat: add getCloudName cloudinary util function

--- lib/__tests__/cloudinary-utils.test.ts
+++ lib/__tests__/cloudinary-utils.test.ts
@@ -1,4 +1,4 @@
-import { getUniquePath } from "../image/cloudinary-utils";
+import { getUniquePath, getCloudName } from "../image/cloudinary-utils";
 
 const CLOUDINARY_URL_EXAMPLES = {
   basic:
@@ -7,12 +7,14 @@ const CLOUDINARY_URL_EXAMPLES = {
     "https://res.cloudinary.com/wargabantuwarga/image/upload/c_scale,w_720,q_90,cs_tinysrgb,f_auto/v1627049958/hero_banner_desktop_zat71c.png",
   multiTransformed:
     "https://res.cloudinary.com/wargabantuwarga/image/upload/c_scale,w_720/w_900/v1627049958/hero_banner_desktop_zat71c.png",
+  otherCloudName:
+    "https://res.cloudinary.com/some-other-name/image/upload/c_scale,w_720/w_900/v1627049958/hero_banner_desktop_zat71c.png",
   invalid: "res.cloudinary.com/",
 };
 
 const CLOUDINARY_END_PATH = "v1627049958/hero_banner_desktop_zat71c.png";
 
-describe("cloudinary-utils", () => {
+describe("getUniquePath", () => {
   it.each`
     input                                       | expected
     ${CLOUDINARY_URL_EXAMPLES.basic}            | ${CLOUDINARY_END_PATH}
@@ -25,6 +27,24 @@ describe("cloudinary-utils", () => {
     "should return '$expected' when '$input' is provided",
     ({ input, expected }) => {
       expect(getUniquePath(input as string)).toBe(expected);
+    },
+  );
+});
+
+describe("getCloudName", () => {
+  it.each`
+    input                                       | expected
+    ${CLOUDINARY_URL_EXAMPLES.basic}            | ${"wargabantuwarga"}
+    ${CLOUDINARY_URL_EXAMPLES.transformed}      | ${"wargabantuwarga"}
+    ${CLOUDINARY_URL_EXAMPLES.multiTransformed} | ${"wargabantuwarga"}
+    ${CLOUDINARY_URL_EXAMPLES.otherCloudName}   | ${"some-other-name"}
+    ${CLOUDINARY_URL_EXAMPLES.invalid}          | ${null}
+    ${null}                                     | ${null}
+    ${123}                                      | ${null}
+  `(
+    "should return '$expected' when '$input' is provided",
+    ({ input, expected }) => {
+      expect(getCloudName(input as string)).toBe(expected);
     },
   );
 });
--- lib/image/cloudinary-utils.ts
+++ lib/image/cloudinary-utils.ts
@@ -4,5 +4,11 @@ export const getUniquePath = (fullUrl: string = "") => {
   return splitPath ? `v${splitPath}` : null;
 };
 
+export const getCloudName = (fullUrl: string = "") => {
+  if (!fullUrl || typeof fullUrl !== "string") return null;
+  const splitPath = fullUrl.split("res.cloudinary.com/")[1];
+  return splitPath ? splitPath.split("/")[0] : null;
+};
+
 export const grayBlur =
   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII=";
