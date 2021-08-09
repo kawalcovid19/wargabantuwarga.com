791d8d3cc279b516a16a61a2bce84a284f232579
Kurnianto Trilaksono <kurnianto@connectedfreight.com>
2021-08-09 13:31:40 +0700

    feat: remove last updated alert from homepage - remove test altogether

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,23 +1,7 @@
-import React from "react";
-
-import { render, screen } from "@testing-library/react";
-import { latestNewsItemBuilder } from "~/lib/content/__mocks__/builders/informasi-terbaru";
-import HomePage, { getStaticProps } from "~/pages";
+import { getStaticProps } from "~/pages";
 
 jest.mock("~/lib/content/home-page");
 jest.mock("~/lib/content/welcome-message");
-
-describe("HomePage", () => {
-  it("the last updated alert is not rendered anymore", () => {
-    render(<HomePage latestNews={[latestNewsItemBuilder()]} />);
-
-    const LastUpdatedAlert = screen.queryByText(
-      /Pembaruan terakhir pada Selasa, 27 Juli 2021 17.43 WIB/i,
-    );
-
-    expect(LastUpdatedAlert).not.toBeInTheDocument();
-  });
-});
 
 describe("getStaticProps", () => {
   // TODO: (ZF) Find a way to load the markdown files in Jest so that we can test the successful case.
