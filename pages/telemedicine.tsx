a2964b9c1ca28c55bc3650ae75b6fb27811691e3
Hani Husamuddin <hani.husam@gmail.com>
2021-08-09 09:44:08 +0700

    feat(ui): create telemedicine page with dummy data

---
+++ pages/telemedicine.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+import { NextSeo } from "next-seo";
+import { GetStaticProps } from "next";
+import { Page } from "~/components/layout/page";
+import { PageHeader } from "~/components/layout/page-header";
+import { BackButton } from "~/components/layout/back-button";
+import { TelemedicineContent } from "~/components/telemedicine/telemedicine-content";
+import TelemedicineContactSection from "~/components/telemedicine/telemedicine-contact-section";
+import { Container } from "~/components/ui/container";
+import emergencyContacts, { Contact } from "~/lib/content/emergency-contacts";
+
+type TelemedicineProps = {
+  emergencyContacts: Contact;
+};
+
+const meta = {
+  title: `Telemedicine Gratis (Inisiatif Beberapa Dokter)`,
+  description: `Konsultasi medis KHUSUS PASIEN COVID-19 yang sedang ISOLASI MANDIRI. Konsultasi yang diberikan hanya konsultasi medis, TIDAK MENYEDIAKAN AMBULANS atau TEMPAT di RUMAH SAKIT. Informasi ini didapat dari sosial media @floraacii. Contact person: dr. Riyo Pungki Irawan (081902835039)`,
+};
+
+export default function TelemedicinePage(props: TelemedicineProps) {
+  return (
+    <Page>
+      <NextSeo
+        description={meta.description}
+        openGraph={{ title: meta.title, description: meta.description }}
+        title={meta.title}
+      />
+      <PageHeader
+        backButton={<BackButton href="/" />}
+        breadcrumbs={[
+          {
+            name: "Telemedicine Gratis",
+            href: "/telemedicine",
+            current: true,
+          },
+        ]}
+        description={meta.description}
+        title={meta.title}
+      />
+      <TelemedicineContent>
+        <Container className="space-y-2">
+          <TelemedicineContactSection
+            emergency_contacts={props.emergencyContacts.emergency_contacts}
+          />
+        </Container>
+      </TelemedicineContent>
+    </Page>
+  );
+}
+
+export const getStaticProps: GetStaticProps = () => {
+  return {
+    props: {
+      emergencyContacts,
+    },
+  };
+};
