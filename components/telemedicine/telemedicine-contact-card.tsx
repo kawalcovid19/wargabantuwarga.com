287b9e8c1fa19ad5504549a0abf21daacf31d440
Hani Husamuddin <hani.husam@gmail.com>
2021-08-09 11:49:06 +0700

    refactor(ui): revamp contact card layout

--- components/telemedicine/telemedicine-contact-card.tsx
+++ components/telemedicine/telemedicine-contact-card.tsx
@@ -1,37 +1,55 @@
+import { ChatAlt2Icon, ClockIcon } from "@heroicons/react/solid";
 import React from "react";
 
 import { PrimaryAnchorButton } from "~/components/ui/button";
-import { ContactDetail } from "~/lib/content/emergency-contacts";
+import { ContactDetail } from "~/lib/content/telemedicine-contacts";
 
 export function TelemedicineContactCard(contact: ContactDetail) {
   return (
     <div className="flex flex-col shadow-md rounded-md overflow-hidden">
-      <div className="flex-shrink-0 py-2 px-3 md:p-4">
-        <img
-          alt={`kontak darurat covid ${contact.name}`}
-          className="h-24 object-scale-down w-full"
-          src={contact.image}
-        />
-      </div>
-      <div className="p-3 text-xs space-y-1 h-0 flex-1">
-        <p className="text-gray-500">{contact.name}</p>
-        <h3 className="font-semibold text-gray-700 text-xs">
-          {contact.description}
-        </h3>
+      <div className="flex justify-content-between">
+        <div className="flex-shrink-0 py-2 px-3 md:p-4">
+          <img
+            alt={`kontak dokter ${contact.doctor_name}`}
+            className="h-24 w-24 rounded-full"
+            src="https://i.pravatar.cc"
+          />
+        </div>
+        <div className="p-3 text-xs space-y-1 h-0 flex-1">
+          <p className="font-semibold text-gray-700">{contact.doctor_name}</p>
+          <div className="mt-2 flex justify-between w-full">
+            <p className="flex items-center text-sm text-gray-500">
+              <ClockIcon
+                aria-hidden="true"
+                className="flex-shrink-0 mr-2 h-4 w-4 text-gray-400"
+              />
+              {`${contact.ops_date} • ${contact.ops_time}`}
+            </p>
+          </div>
+          <div className="mt-2 flex justify-between w-full">
+            <p className="flex items-center text-sm text-gray-500">
+              <ChatAlt2Icon
+                aria-hidden="true"
+                className="flex-shrink-0 mr-2 h-4 w-4 text-gray-400"
+              />
+              {contact.platform}
+            </p>
+          </div>
+        </div>
       </div>
       <div className="p-3 justify-center">
         <PrimaryAnchorButton
           block
           className="bg-light-blue-400 font-normal focus:bg-light-blue hover:bg-light-blue-600 text-white"
           color="none"
-          data-testid={`contact-button-${contact.name}`}
-          href={contact.url}
+          data-testid={`contact-button-${contact.doctor_name}`}
+          href={contact.contact}
           rel="nofollow noopener noreferrer"
           rounded
           size="xs"
           target="_blank"
         >
-          Cek Sekarang
+          Hubungi
         </PrimaryAnchorButton>
       </div>
     </div>
