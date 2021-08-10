2831270dc8c31e9196ec46d585b0d6f070b2d1ea
Nasrul Gunawan <nasrul.gunawan@nutrifood.co.id>
2021-08-10 18:20:48 +0700

    Add extra padding

--- components/ui/feedback-section.tsx
+++ components/ui/feedback-section.tsx
@@ -5,9 +5,9 @@ import { DISCUSSION_URL } from "~/lib/constants/link";
 
 export function FeedbackSection() {
   return (
-    <div className="flex-1 px-4 pb-2">
+    <div className="flex-1 px-4 pt-4 pb-12">
       <Container>
-        <div className="p-4 my-2 bg-white overflow-hidden shadow rounded-md">
+        <div className="p-4 bg-white overflow-hidden shadow rounded-md">
           <h2 className="text-center font-semibold text-gray-700 text-lg">
             Ada usulan / laporan terkait website ini?
           </h2>
