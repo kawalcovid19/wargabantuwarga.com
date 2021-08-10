58e483d7074b631cf282d9e2b3593ca194d9c85b
Nasrul Gunawan <nasrul.gunawan@nutrifood.co.id>
2021-08-10 18:44:09 +0700

    Fixed test

--- components/layout/__tests__/navigation.test.tsx
+++ components/layout/__tests__/navigation.test.tsx
@@ -53,6 +53,7 @@ describe("Navigation", () => {
       >
         <a
           class="inline-flex flex-col items-center justify-center text-center h-12 px-2 rounded-md text-gray-600 hover:text-blue-600"
+          href="https://bit.ly/hotlinewarga"
           rel="nofollow noopener noreferrer"
           target="_blank"
         >
