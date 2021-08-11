389acf928ba6c349e2d42e1a39984063aaaf2e95
Hani Husamuddin <hani.husam@gmail.com>
2021-08-11 20:50:30 +0700

    test(fix): get contact link fn test

--- lib/home/__test__/get-contact-links.test.ts
+++ lib/home/__test__/get-contact-links.test.ts
@@ -2,28 +2,19 @@ import { getContactLink } from "../get-contact-link";
 
 type Platform = "instagram" | "telegram" | "twitter" | "whatsapp";
 
-const getContact = (platform: Platform) => {
-  switch (platform) {
-    case "whatsapp":
-      return "0812345678";
-    default:
-      return "@username";
-  }
-};
-
 describe("getContactLinks", () => {
   it.each`
-    platform       | expected
-    ${"whatsapp"}  | ${`https://wa.me/${new RegExp(/^628/, "i")}`}
-    ${"telegram"}  | ${`https://t.me/${new RegExp(/^[a-zA-Z][0-9]{4}$/)}`}
-    ${"instagram"} | ${`https://instagram.com/${new RegExp(/[a-zA-Z&&[^@]]+/)}`}
-    ${"twitter"}   | ${`https://twitter.com/${new RegExp(/[a-zA-Z&&[^@]]+/)}`}
+    platform       | contact         | expected
+    ${"whatsapp"}  | ${"0812345678"} | ${`https://wa.me/${new RegExp(/^628/, "i")}`}
+    ${"telegram"}  | ${"@username"}  | ${`https://t.me/${new RegExp(/^[a-zA-Z][0-9]{4}$/)}`}
+    ${"instagram"} | ${"@username"}  | ${`https://instagram.com/${new RegExp(/[a-zA-Z&&[^@]]+/)}`}
+    ${"twitter"}   | ${"@username"}  | ${`https://twitter.com/${new RegExp(/[a-zA-Z&&[^@]]+/)}`}
   `(
     "returns $expected contact url when platform is $platform",
-    ({ platform, expected }) => {
-      expect(
-        getContactLink(platform as Platform, getContact(platform as Platform)),
-      ).toEqual(expected);
+    ({ platform, contact, expected }) => {
+      expect(getContactLink(platform as Platform, contact as string)).toEqual(
+        expected,
+      );
     },
   );
 });
