cd16879fe89d872734da06f8e203e5185aac801d
Irfan Maulana <mazipanneh@gmail.com>
2021-08-13 10:11:52 +0700

    feat: edukasi entry point

--- components/home/__tests__/homepage-start.test.tsx
+++ components/home/__tests__/homepage-start.test.tsx
@@ -301,6 +301,41 @@ describe("HomePageStart", () => {
           <svg
             class="h-6 w-6 text-brand-500"
             fill="none"
+<<<<<<< HEAD
+=======
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.752 2.5a1 1 0 011-1h.684c.038 0 .39 0 .764.002.52.002.92.408.92.929v5.494l-1.313-.787a.722.722 0 00-.742 0l-1.313.787V2.5z"
+              fill="currentColor"
+            />
+            <path
+              clip-rule="evenodd"
+              d="M3.5 5.254v14.455A2.791 2.791 0 006.291 22.5h11.551a2.791 2.791 0 002.792-2.791V5.254a3.75 3.75 0 00-1.564-3.049.866.866 0 00-1.012 1.407c.512.368.843.966.843 1.642v10.699a3.771 3.771 0 00-.578-.044H5.81c-.196 0-.389.015-.577.044V5.254c0-1.116.905-2.021 2.021-2.021h5.053a.866.866 0 000-1.733H7.254A3.754 3.754 0 003.5 5.254zm1.733 13.492v.963c0 .584.474 1.058 1.058 1.058h11.551c.45 0 .833-.279.987-.673H7.254a.866.866 0 010-1.733h11.575a1.06 1.06 0 00-.987-.674H6.292c-.585 0-1.06.474-1.06 1.059z"
+              fill="currentColor"
+              fill-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <a
+          class="text-gray-900 font-semibold text-xs mt-3 helper-link-cover"
+          href="/edukasi"
+        >
+          Edukasi
+        </a>
+      </li>
+      <li
+        class="inline-flex flex-col items-center justify-center text-center relative shadow-lg rounded-lg py-3 px-2"
+      >
+        <div
+          aria-hidden="true"
+          class="flex items-center justify-center h-10 w-10 bg-blue-100 rounded-full"
+        >
+          <svg
+            class="h-6 w-6 text-brand-500"
+            fill="none"
+>>>>>>> bed9dfd (feat: edukasi entry point)
             height="24"
             viewBox="0 0 24 24"
             width="24"
@@ -384,7 +419,11 @@ describe("HomePageStart", () => {
           alt="Ajukan bantuan sembako jika positif Covid-19 - Daftar Sekarang"
           decoding="async"
           src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+<<<<<<< HEAD
           style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; box-sizing: border-box; padding: 0px; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"
+=======
+          style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; box-sizing: border-box; padding: 0px; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%; filter: blur(20px); background-size: cover; background-image: url(https://res.cloudinary.com/wargabantuwarga/image/upload/c_scale,w_656,q_2,f_auto,e_blur:1000/v1627319803/sembako-cta-v2_czojls);"
+>>>>>>> bed9dfd (feat: edukasi entry point)
         />
       </div>
     </a>
