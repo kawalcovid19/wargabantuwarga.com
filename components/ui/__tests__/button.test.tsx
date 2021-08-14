88ce9125fca035346ce7fd324c34436f5c105af9
adibfirman <dib.firman@gmail.com>
2021-08-14 21:48:25 +0700

    test: replacing toMatchInlineSnapshot within component/ui/__tests__/* files

--- components/ui/__tests__/button.test.tsx
+++ components/ui/__tests__/button.test.tsx
@@ -85,18 +85,9 @@ describe("Button", () => {
 
   describe("SecondaryButton", () => {
     it("renders correctly", () => {
-      const { container } = render(
-        <SecondaryButton>test button</SecondaryButton>,
-      );
+      render(<SecondaryButton>test button</SecondaryButton>);
 
-      expect(container.firstChild).toMatchInlineSnapshot(`
-        <button
-          class="inline-flex flex-row px-4 py-2 text-sm rounded-md items-center justify-center border border-transparent font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 text-blue-700 bg-blue-100 hover:bg-blue-200 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-75"
-          type="button"
-        >
-          test button
-        </button>
-      `);
+      expect(screen.getByText("test button")).toBeInTheDocument();
     });
 
     it("renders with correct colors", () => {
@@ -161,16 +152,9 @@ describe("Button", () => {
 
   describe("OutlineButton", () => {
     it("renders correctly", () => {
-      const { container } = render(<OutlineButton>test button</OutlineButton>);
+      render(<OutlineButton>test button</OutlineButton>);
 
-      expect(container.firstChild).toMatchInlineSnapshot(`
-        <button
-          class="inline-flex flex-row px-4 py-2 text-sm rounded-md items-center justify-center border border-transparent font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 text-blue-500 border-blue-500 hover:bg-blue-100 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-75"
-          type="button"
-        >
-          test button
-        </button>
-      `);
+      expect(screen.getByText("test button")).toBeInTheDocument();
     });
 
     it("renders with correct colors", () => {
