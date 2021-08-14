1d1afbd6c493e2790fc450077c8a66034c0af743
Zain Fathoni <me@zainf.dev>
2021-08-14 22:19:23 +0800

    test(e2e): visit pages and query textboxes in a better way

--- cypress/e2e/provinces/search.js
+++ cypress/e2e/provinces/search.js
@@ -1,10 +1,12 @@
 describe("provinces page typed search", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/provinces");
+    cy.visit("/provinces");
   });
 
   it("gives only one result of Sumatera Utara when search keyword is Sumatera", () => {
-    cy.get("input#keywordsInput").type("Sumatera Utara");
+    cy.findByRole("textbox", {
+      name: /cari provinsi:/i,
+    }).type("Sumatera Utara");
     cy.get("ul").first().children().should("have.length", 1);
     cy.get("ul")
       .first()
@@ -14,7 +16,9 @@ describe("provinces page typed search", () => {
   });
 
   it("gives 3 result of Jawa Barat, Tengah, Timur when search keyword is Jawa", () => {
-    cy.get("input#keywordsInput").type("Jawa");
+    cy.findByRole("textbox", {
+      name: /cari provinsi:/i,
+    }).type("Jawa");
     cy.get("ul").first().children().should("have.length", 3);
     cy.get("ul")
       .first()
@@ -25,14 +29,16 @@ describe("provinces page typed search", () => {
   });
 
   it("gives no result when search keyword does not correspond to any provinces in Indonesia", () => {
-    cy.get("input#keywordsInput").type("asdf");
+    cy.findByRole("textbox", {
+      name: /cari provinsi:/i,
+    }).type("asdf");
     cy.contains("Provinsi tidak ditemukan").should("exist");
   });
 });
 
 describe("provinces page query param search", () => {
   it("gives only one result of DKI Jakarta when search keyword is dki", () => {
-    cy.visit("http://localhost:3000/provinces?q=dki");
+    cy.visit("/provinces?q=dki");
 
     cy.get("ul").first().children().should("have.length", 1);
     cy.get("ul")
@@ -43,7 +49,7 @@ describe("provinces page query param search", () => {
   });
 
   it("gives 5 result of Sulawesi Utara, Tengah, Selatan, Tenggara, Barat when search keyword is sulawesi", () => {
-    cy.visit("http://localhost:3000/provinces?q=sulawesi");
+    cy.visit("/provinces?q=sulawesi");
 
     cy.get("ul").first().children().should("have.length", 5);
     cy.get("ul")
@@ -55,7 +61,7 @@ describe("provinces page query param search", () => {
   });
 
   it("gives no result when search keyword does not correspond to any provinces in Indonesia", () => {
-    cy.visit("http://localhost:3000/provinces?q=asdf");
+    cy.visit("/provinces?q=asdf");
 
     cy.contains("Provinsi tidak ditemukan").should("exist");
   });
