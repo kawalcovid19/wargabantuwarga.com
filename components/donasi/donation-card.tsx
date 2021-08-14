038c727762026ce6252275a9f22a4b2169d1449e
Hani Husamuddin <hani.husam@gmail.com>
2021-08-14 21:03:59 +0700

    refactor(ui): change donasi image url

--- components/donasi/donation-card.tsx
+++ components/donasi/donation-card.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import Image from "next/image";
 
 import { PrimaryAnchorButton } from "~/components/ui/button";
 import { DonationDetail } from "~/lib/content/donasi";
+import { cloudinaryLoader } from "~/lib/image/cloudinary-loader";
+import { replaceCloudinaryPrefix } from "~/lib/image/cloudinary-utils";
 
 export function DonationCard(donation: DonationDetail) {
   return (
     <div className="flex flex-col shadow-md rounded-md overflow-hidden max-w-xs">
       <div className="flex-shrink-0">
-        <div className="h-24 object-cover w-full md:h-32 bg-gray-500" />
+        <Image
+          alt="donasi covid"
+          className="shadow rounded-md"
+          height={397}
+          layout="intrinsic"
+          loader={cloudinaryLoader}
+          loading="lazy"
+          quality={90}
+          src={replaceCloudinaryPrefix(donation.image)}
+          width={720}
+        />
       </div>
       <div className="p-3 space-y-1 h-0 flex-1">
         <p className="text-gray-500 text-xs sm:text-sm">{donation.category}</p>
