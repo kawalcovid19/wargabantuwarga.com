5f77b2175ae1d7f813fdaa4c7e29476aac3645b3
Adib Firman <adib.firman@tokopedia.com>
2021-08-16 06:26:27 +0700

    test: replace toMatchInlineSnapshot with meaningful assertions (#728)
    * test: replace toMatchInlineSnapshot on component/report-button test
    
    * test: replacing toMatchInlineSnapshot within component/__tests__/* files
    
    * test: replacing toMatchInlineSnapshot within component/home__tests__/* files
    
    * test: replacing toMatchInlineSnapshot within component/kontak-darurat/__tests__/* files
    
    * test: replacing toMatchInlineSnapshot within component/layout/__tests__/* files
    
    * test: replacing toMatchInlineSnapshot within component/ui/__tests__/* files
    
    * test: replacing toMatchInlineSnapshot within component/ui/__tests__/* files
    
    * test: improve from input CR
    
    * test: improve from input CR
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- components/ui/__tests__/feedback-section.test.tsx
+++ components/ui/__tests__/feedback-section.test.tsx
@@ -1,48 +1,16 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { FeedbackSection } from "~/components/ui/feedback-section";
 
 describe("FooterDiscussionSection", () => {
   it("renders correctly", () => {
-    const { container } = render(<FeedbackSection />);
+    render(<FeedbackSection />);
+    const textSection = screen.getByText("Sampaikan masukan Anda");
+    const anchorTextSection = textSection.closest("a");
 
-    expect(container.firstChild).toMatchInlineSnapshot(`
-      <div
-        class="px-4 overflow-hidden bg-white pt-6 pb-24 space-y-2 sm:pb-6"
-      >
-        <h2
-          class="text-center font-semibold text-gray-700 text-lg"
-        >
-          Ada usulan / laporan terkait website ini?
-        </h2>
-        <div
-          class="flex justify-center"
-        >
-          <a
-            aria-label="Sampaikan masukan Anda"
-            class="inline-flex flex-row px-4 py-2 text-sm rounded-full items-center justify-center border border-transparent font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-75 mt-2"
-            href="https://kcov.id/wbw-discuss"
-            rel="nofollow noopener noreferrer"
-            target="_blank"
-          >
-            <svg
-              aria-hidden="true"
-              class="-ml-1 mr-2 h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-              />
-            </svg>
-            Sampaikan masukan Anda
-          </a>
-        </div>
-      </div>
-    `);
+    expect(textSection).toBeVisible();
+    expect(anchorTextSection).toHaveAttribute(
+      "href",
+      "https://kcov.id/wbw-discuss",
+    );
   });
 });
