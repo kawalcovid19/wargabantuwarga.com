b6007b6ecadf3d8c040387b5d621865cd668218f
Resi Respati <resir014@gmail.com>
2021-08-16 22:01:47 +0700

    chore: migrate tailwind config to `tailwind-preset-wbw` (#752)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,55 +4,16 @@ module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
+  presets: [require("@kawalcovid19/tailwind-preset-wbw")],
   theme: {
     extend: {
-      colors: {
-        // WargaBantuWarga.com brand colour
-        brand: {
-          DEFAULT: "#1667c2",
-          50: "#c5ddf8",
-          100: "#aed0f6",
-          200: "#80b5f1",
-          300: "#539beb",
-          400: "#2580e6",
-          500: "#1667c2",
-          600: "#114f94",
-          700: "#0c3666",
-          800: "#061e39",
-          900: "#01060b",
-        },
-        "light-blue": {
-          DEFAULT: "#0ea5e9",
-          50: "#f0f9ff",
-          100: "#e0f2fe",
-          200: "#bae6fd",
-          300: "#7dd3fc",
-          400: "#38bdf8",
-          500: "#0ea5e9",
-          600: "#0284c7",
-          700: "#0369a1",
-          800: "#075985",
-          900: "#0c4a6e",
-        },
-      },
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            hr: {
-              borderColor: "#9ca3af",
-              marginTop: "1.5rem",
-              marginBottom: "1.5rem",
-            },
-          },
-        },
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [],
 };
