26fb2e4cd09b77b514abb1bcc58500c5a7d0e3d8
dekwahdimas <ign.bagusdimasw@gmail.com>
2021-08-17 19:50:43 +0800

    restore deleted line and reupload new files after reset

---
+++ __tests__/pages/telekonseling.test.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import TelecounselingPage, { getStaticProps } from "~/pages/telekonseling";
+import telekonseling from "~/lib/content/telekonseling";
+
+jest.mock("~/lib/content/telekonseling");
+jest.mock("next/router", () => require("next-router-mock"));
+
+describe("TelecounselingPage", () => {
+  const { supports } = telekonseling;
+
+  it("renders the title, description and breadcrumbs correctly", async () => {
+    render(<TelecounselingPage telekonseling={telekonseling} />);
+
+    const title = screen.getByRole("heading", {
+      name: /Curhat atau Telekonseling/i,
+    });
+    expect(title).toBeVisible();
+
+    const description = screen.getByText(
+      /Informasi mengenai relawan COVID-19 yang memberikan bantuan berupa layanan konsultasi psikologi, kesehatan, agama, dan lainnya yang relevan untuk membantu kesehatan mental terdampak COVID-19.$/i,
+    );
+    expect(description).toBeVisible();
+
+    const breadcrumbs = screen.getByRole("link", {
+      name: "Curhat/Telekonseling",
+    });
+    // const breadcrumbs = screen.getByText(/^Curhat atau Telekonseling$/i);
+    expect(breadcrumbs).toBeVisible();
+    expect(breadcrumbs).toHaveAttribute("href", "/telekonseling");
+    // breadcrumbs.getAttribute("href") === "telekonseling";
+  });
+
+  it("renders the supports correctly", () => {
+    render(<TelecounselingPage telekonseling={telekonseling} />);
+
+    supports.forEach((support) => {
+      expect(screen.getByText(support.support)).toBeVisible();
+
+      support.contents.forEach((content) => {
+        const link = screen.getByRole("link", { name: /{content.title}/i });
+
+        expect(screen.getByText(content.title)).toBeVisible();
+        expect(link).toBeVisible();
+        expect(link).toHaveAttribute("href", content.url);
+        expect(
+          screen.getByRole("link", { name: /{content.title}/i }),
+        ).toBeVisible();
+      });
+    });
+  });
+});
+
+describe("getStaticProps", () => {
+  it("returns the props from the telecounseling contents correctly", () => {
+    expect(getStaticProps({})).toEqual({
+      props: { telekonseling },
+    });
+  });
+});
---
+++ components/telekonseling/telekonseling-link-section.tsx
@@ -0,0 +1,24 @@
+import { InternalPageSection } from "~/components/layout/page";
+import StackedLink from "~/components/layout/stacked-link";
+import { Telecounseling } from "~/lib/content/telekonseling";
+
+interface TelecounselingSectionProps {
+  telekonseling: Telecounseling;
+}
+
+export function TelecounselingSection(props: TelecounselingSectionProps) {
+  return (
+    <InternalPageSection className="flex-1 pb-6 space-y-8">
+      {props.telekonseling.supports.map((counseling, index) => (
+        <div key={index} className="space-y-4">
+          <h2 className="text-base font-semibold text-gray-900">
+            {counseling.support}
+          </h2>
+          <div className="p-2 bg-gray-50 rounded-md">
+            <StackedLink links={counseling.contents} />
+          </div>
+        </div>
+      ))}
+    </InternalPageSection>
+  );
+}
