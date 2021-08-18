7076efe8395f12258fb21ef85cfca03e5817d61d
dekwahdimas <ign.bagusdimasw@gmail.com>
2021-08-18 11:34:03 +0800

    avoid using index as suggested

--- components/telekonseling/telekonseling-link-section.tsx
+++ components/telekonseling/telekonseling-link-section.tsx
@@ -9,8 +9,8 @@ interface TelecounselingSectionProps {
 export function TelecounselingSection(props: TelecounselingSectionProps) {
   return (
     <InternalPageSection className="flex-1 pb-6 space-y-8">
-      {props.telekonseling.supports.map((counseling, index) => (
-        <div key={index} className="space-y-4">
+      {props.telekonseling.supports.map((counseling) => (
+        <div key={counseling.support} className="space-y-4">
           <h2 className="text-base font-semibold text-gray-900">
             {counseling.support}
           </h2>
