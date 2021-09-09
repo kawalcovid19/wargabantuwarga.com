708fc3b096a789d964d66d6eb0ae7ed0cc715cec
asaadam <graha.asa@gmail.com>
2021-09-09 20:40:29 +0700

    fix: exclude mirror-box in jest, and fix download prefetched-json

--- etc/mirror-box.ts
+++ etc/mirror-box.ts
@@ -4,7 +4,6 @@ import chalk from "chalk";
 import fetch from "cross-fetch";
 import ora from "ora";
 import { toSecond } from "../lib/string-utils";
-import { fetchDatabase } from "./fetchers/fetch-database";
 
 export async function mirror(name: string) {
   const start = process.hrtime();
@@ -17,25 +16,8 @@ export async function mirror(name: string) {
   });
 }
 
-export async function mirrorDatabase() {
-  const start = process.hrtime();
-  const spinner = ora(`${chalk.yellowBright("Mirroring Database")}`).start();
-  spinner.start(`${chalk.yellowBright("Fetching next data...")}`);
-
-  fetchDatabase()
-    .then(() => {
-      const end = `${toSecond(process.hrtime(start))} seconds`;
-      spinner.succeed(
-        `Finished mirroring Database in ${chalk.greenBright(end)}`,
-      );
-    })
-    .catch((err) => {
-      chalk.red(err);
-    });
-}
-
 (function main() {
   void mirror("wbw-faq-sheets.json");
   void mirror("wbw-sheets.json");
-  void mirrorDatabase();
+  void mirror("wbw-database.json");
 })();
