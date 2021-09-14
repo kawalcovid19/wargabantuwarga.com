590abc2ca42817e3db05fd8212fda95fd11ec8fb
Zain Fathoni <me@zainf.dev>
2021-09-14 23:47:35 +0800

    refactor(e2e): extract forEachContactItem custom command

--- cypress/e2e/contacts/search.js
+++ cypress/e2e/contacts/search.js
@@ -12,14 +12,8 @@ describe("contacts page typed search", () => {
       name: /cari/i,
     }).click();
 
-    cy.findByTestId("contact-list").within(() =>
-      cy
-        .get("li")
-        .each((contact) =>
-          cy
-            .wrap(contact)
-            .within(() => cy.findByText("08111617101").should("be.visible")),
-        ),
+    cy.forEachContactItem(() =>
+      cy.findByText("08111617101").should("be.visible"),
     );
   });
 
@@ -28,14 +22,8 @@ describe("contacts page typed search", () => {
       name: /cari kontak:/i,
     }).type("Sudirman{enter}");
 
-    cy.findByTestId("contact-list").within(() =>
-      cy
-        .get("li")
-        .each((contact) =>
-          cy
-            .wrap(contact)
-            .within(() => cy.findAllByText(/sudirman/i).should("be.visible")),
-        ),
+    cy.forEachContactItem(() =>
+      cy.findAllByText(/sudirman/i).should("be.visible"),
     );
   });
 
@@ -44,16 +32,8 @@ describe("contacts page typed search", () => {
       name: /cari kontak:/i,
     }).type("dompet dhuafa{enter}");
 
-    cy.findByTestId("contact-list").within(() =>
-      cy
-        .get("li")
-        .each((contact) =>
-          cy
-            .wrap(contact)
-            .within(() =>
-              cy.findAllByText(/dompet dhuafa/i).should("be.visible"),
-            ),
-        ),
+    cy.forEachContactItem(() =>
+      cy.findAllByText(/dompet dhuafa/i).should("be.visible"),
     );
   });
 });
