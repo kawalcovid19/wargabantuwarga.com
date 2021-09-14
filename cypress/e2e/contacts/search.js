ed1e543acbd514b2cb6518e3a638ca9e38210924
Zain Fathoni <me@zainf.dev>
2021-09-14 23:23:26 +0800

    test(e2e): search by phone number

---
+++ cypress/e2e/contacts/search.js
@@ -0,0 +1,25 @@
+describe("contacts page typed search", () => {
+  beforeEach(() => {
+    cy.visit("/provinces/dki-jakarta");
+  });
+
+  it("search by phone number", () => {
+    cy.findByRole("textbox", {
+      name: /cari kontak:/i,
+    }).type("08111617101");
+
+    cy.findByRole("button", {
+      name: /cari/i,
+    }).click();
+
+    cy.findByTestId("contact-list").within(() =>
+      cy
+        .get("li")
+        .each((contact) =>
+          cy
+            .wrap(contact)
+            .within(() => cy.findByText("08111617101").should("be.visible")),
+        ),
+    );
+  });
+});
