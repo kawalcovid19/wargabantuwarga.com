93b1f76e20bac424688e16c3ab4ce27d98c4e8fc
FortressValkyrie <nefothingy@hotmail.com>
2021-10-26 20:34:14 +0700

    feat(fetcher): implement vaksinasi.id db fetcher (#806)
    * feat(fetcher): implement vaksinasi.id db fetcher
    
    Fetches vaksinasi.id's DB and store it in wbw-vaccination-database.json
    
    Refs: #803
    
    * style(fetcher): sort union type members in fetch-vaccine-database.ts
    
    There is an ESLint warning in c84e78c, this commit addresses that warn
    
    * refactor(fetcher): move types of vaksinid fetcher to lib
    
    * refactor(fetcher): move the vaksin.id url to the top
    
    * feat(fetcher): fetch vaksinasi.id db in fetch-wbw.ts
    
    * refactor(fetcher): reduce repetition in vaccination types
    
    * fix(fetcher): fix program stuck caused by spinner
    
    fetchVaccinationDatabase() is so fast that a spinner stucks the program.
    
    So, move it to the top and remove line 37.
    
    * refactor(fetcher): transform vaksinid output data to be Contact-compatible
    
    Transforms the output of the vaccination db fetcher to VaccinationContact[].
    
    This will make it ready to be used downstream in the components.
    
    The VaccinationContact interface has a 'map' property.
    This is unecessary as WBW's "Buka Peta" button uses the address instead.
    Though, I still add it in case we want to use that for the "Buka Peta" button.
    
    * refactor(fetcher): change vaksin.id output to PartialVaccinationProvince[]
    
    Make it easy for the DB to be merged with the other contact DBs.
    
    Using VaccinationContact[] might force us into unnecessarily iterating the array just to merge it.
    
    * refactor(fetcher): change vaksin.id output to an object
    
    Using an object provide an easier iteration than PartialProvinceContact[].
    
    PartialProvinceContact is also removed because it's now useless.
    
    Refs: #806 issue comment 940051302
    
    * chore: switch netlify export to yarn fetch-wbw
    
    * feat(fetcher): log some info from  vaccination db
    
    * feat(fetcher): log the output text of vaccine.db fetcher
    
    * fix(fetcher): fix vaksinasi.id fetcher only fetching 4 provinces
    
    * refactor(fetcher): remove console.log()s from vaccination fetcher

---
+++ lib/data/vaccination.ts
@@ -0,0 +1,53 @@
+import { Contact } from "./provinces";
+
+type City = `${"Kab." | "Kota"} ${string}`;
+type DateString = `${number}-${number}-${number}`;
+type TimeString = `${number}:${number}:${number}`;
+type VaccinationAgeRange = Array<`${string} (${number}-${
+  | number
+  | null} Tahun)`>;
+
+export interface VaccinationRegions {
+  province: string;
+  city: Array<City>;
+}
+
+export interface VaccinationRegionsResponse {
+  data: Array<VaccinationRegions>;
+}
+export interface VaccinationLocation {
+  province: string;
+  city: City;
+  title: string;
+  datestart: DateString;
+  dateend: DateString;
+  timestart: TimeString;
+  timeend: TimeString;
+  registration: string;
+  agerange: VaccinationAgeRange;
+  description: string;
+  link: string;
+  address: string;
+  map: string;
+  isFree: boolean;
+  isvalid: boolean;
+  code: string;
+  dateadded: DateString;
+}
+
+type _location = VaccinationLocation;
+export interface VaccinationRegion {
+  data: Array<VaccinationLocation>;
+}
+
+export interface VaccinationContact extends Contact {
+  rentang_umur: _location["agerange"];
+  buka_waktu: _location["timestart"];
+  tutup_waktu: _location["timeend"];
+  mulai_tanggal: _location["datestart"];
+  selesai_tanggal: _location["dateend"];
+  kode?: _location["code"];
+  gratis?: _location["isFree"];
+  map: _location["map"];
+  link: _location["link"];
+}
