91f6cbac351e46685d21ea296804cf5654f69f86
Zain Fathoni <me@zainf.dev>
2022-11-05 10:41:36 +0800

    test: htmrTransform for headings tag

--- lib/__tests__/html-transformers.test.tsx
+++ lib/__tests__/html-transformers.test.tsx
@@ -33,12 +33,53 @@ describe("htmrTransform", () => {
 
   it("transform an anchor tag without href property", () => {
     expect(htmrRenderWithTransformer("<a>LBH</a>")).toMatchInlineSnapshot(`
-<a
-  className="text-indigo-600 hover:text-indigo-500"
->
-  LBH
-</a>
-`);
+      <a
+        className="text-indigo-600 hover:text-indigo-500"
+      >
+        LBH
+      </a>
+    `);
+  });
+
+  it("adds kebab-case id to headings correctly", () => {
+    expect(
+      htmrRenderWithTransformer(
+        `<main><h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><h4>Heading 4</h4><h5>Heading 5</h5><h6>Heading 6</h6></main>`,
+      ),
+    ).toMatchInlineSnapshot(`
+      <main>
+        <h1
+          id="heading-1"
+        >
+          Heading 1
+        </h1>
+        <h2
+          id="heading-2"
+        >
+          Heading 2
+        </h2>
+        <h3
+          id="heading-3"
+        >
+          Heading 3
+        </h3>
+        <h4
+          id="heading-4"
+        >
+          Heading 4
+        </h4>
+        <h5
+          id="heading-5"
+        >
+          Heading 5
+        </h5>
+        <h6
+          id="heading-6"
+        >
+          Heading 6
+        </h6>
+      </main>
+    `);
   });
 });
 
