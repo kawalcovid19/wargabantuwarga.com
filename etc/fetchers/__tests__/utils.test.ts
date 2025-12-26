912ec770ac4a18d320f6f670d196dfe4bdedacb6
Zain Fathoni <me@zainf.dev>
2025-12-26 21:23:44 +0700

    refactor: remove unused parseFAQ function
    - Remove deprecated HTML-based parseFAQ function from utils.ts
    - Remove parseFAQ test that's no longer relevant
    - Keep cheerio for extractGoogleQuery (still needed to parse HTML links)
    - Reduce dead code, cleaner exports
    - Tests remain at 32 passing (down from 33)

--- etc/fetchers/__tests__/utils.test.ts
+++ etc/fetchers/__tests__/utils.test.ts
@@ -3,58 +3,10 @@ import cheerio from "cheerio";
 import {
   contactReducer,
   extractGoogleQuery,
-  parseFAQ,
   parseFAQFromCSV,
   parseCSV,
   SheetColumn,
 } from "../utils";
-
-describe("utils > parseFAQ", () => {
-  it("should return correct JSON", async () => {
-    const correctHtml = `
-    <div id="sheets-viewport">
-      <div id="0">
-        <table>
-          <tbody>
-            <tr>
-              <th>Kategori Pertanyaan</th>
-              <th>Pertanyaan</th>
-              <th>Jawaban</th>
-              <th>Created_date</th>
-              <th>Sumber</th>q
-              <th>Link</th>
-              <th>Published Date</th>
-            </tr>
-            <tr>
-              <td>Tanda Bahaya</td>
-              <td>Kapan saya harus ke IGD?</td>
-              <td>Jika ada tanda-tanda seperti:<br>- Sesak nafas &gt;30x per menit, saturasi &lt;90%<br>- Nyeri dada berat atau nyeri dada menetap<br>- Mendadak merasa kebingungan<br>- Mengantuk terus, tidak bisa tetap sadar<br>- Kulit, bibir, atau kuku berubah warna menjadi biru<br>Jika mulai ada gejala-gejala ini, SEGERA KE IGD RUMAH SAKIT. Jika ada gejala lain yang mengkhawatirkan, bisa dikonsultasikan dengan dokter.</td>
-              <td>12 Jul 2021</td>
-              <td>Saran Dokter</td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    `;
-
-    const result = await parseFAQ(correctHtml);
-    expect(result).toStrictEqual([
-      {
-        kategori_pertanyaan: "Tanda Bahaya",
-        pertanyaan: "Kapan saya harus ke IGD?",
-        jawaban:
-          "Jika ada tanda-tanda seperti:<br>- Sesak nafas &gt;30x per menit, saturasi &lt;90%<br>- Nyeri dada berat atau nyeri dada menetap<br>- Mendadak merasa kebingungan<br>- Mengantuk terus, tidak bisa tetap sadar<br>- Kulit, bibir, atau kuku berubah warna menjadi biru<br>Jika mulai ada gejala-gejala ini, SEGERA KE IGD RUMAH SAKIT. Jika ada gejala lain yang mengkhawatirkan, bisa dikonsultasikan dengan dokter.",
-        created_date: "12 Jul 2021",
-        sumber: "Saran Dokter",
-        link: "",
-        published_date: "",
-      },
-    ]);
-  });
-});
 
 describe("utils > parseFAQFromCSV", () => {
   it("should parse simple FAQ CSV", () => {
--- etc/fetchers/utils.ts
+++ etc/fetchers/utils.ts
@@ -17,32 +17,6 @@ export interface ParsedCSV {
   rows: string[][];
 }
 
-export const parseFAQ = async (html: string): Promise<Faqs> => {
-  const $ = cheerio.load(html);
-  const faqRows = $("#sheets-viewport > div#0").find(
-    "table tbody tr:not(:first)",
-  );
-
-  const faqJSON: Faqs = faqRows
-    .map((_, el) => {
-      const row = $(el).find("td");
-      const faq: Faq = {
-        kategori_pertanyaan: $(row.get(0)).text(),
-        pertanyaan: $(row.get(1)).text(),
-        jawaban: $(row.get(2)).html() ?? "",
-        created_date: $(row.get(3)).text(),
-        sumber: $(row.get(4)).text(),
-        link: $(row.get(5)).text(),
-        published_date: $(row.get(6)).text(),
-      };
-
-      return faq;
-    })
-    .toArray();
-
-  return faqJSON;
-};
-
 export const parseFAQFromCSV = (csvText: string): Faqs => {
   const { rows } = parseCSV(csvText);
 
